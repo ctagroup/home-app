d6e8d0ef3563d6dbb45c8256038641fd178a2a31
Blake Ross <mavispuford@gmail.com>
2013-11-30 14:49:51 -0700

    Sorry for the big commit.  Here's what I've done:
    -Vectorized the Housing 1000 logo so it doesn't look so terrible.  I updated the layouts that use it so the sizing doesn't get messed up.
    -Added a DatabaseConnector class that handles database actions.  Has tables for surveys, responses, and response sets
    -Response sets are just for grouping responses so we can queue them up and sync them all at once in the future.  Doesn't have to be used if we don't get around to it
    -Added a SurveyListing class.  I know the Survey one exists already, but it just seemed like their stuff was all over the place...
    -Added a Question class.  This is just so we can parse the JSON into a nice list and then loop through it.
    -Added a RESTHelper class.  This can perform GET/POST actions using the given url and properties.  Properties are passed in as a HashMap<String, String>
    -When the Dynamic Layout button is pressed, the survey list is loaded.  The user can select one from the list, and then it passes that to the ClientInfoActivity_Dynamic class in the Intent extras.  Nothing is done with it yet.

--- housing1000/android/Housing1000/Housing1000/src/main/java/edu/weber/housing1000/JSONParser.java
+++ housing1000/android/Housing1000/Housing1000/src/main/java/edu/weber/housing1000/JSONParser.java
@@ -8,6 +8,9 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import edu.weber.housing1000.data.Question;
+import edu.weber.housing1000.data.SurveyListing;
+
 /**
  * Created by Blake on 11/12/13.
  */
@@ -16,25 +19,30 @@ public class JSONParser {
     public static final String surveys = "[{\"$id\":\"1\",\"SurveyId\":1,\"Title\":\"Santa Clara Survey\",\"Description\":\"This is Santa Clara Survey\",\"CreatedBy\":1,\"CreatedDate\":\"2013-11-05T12:04:23\",\"UpdatedBy\":null,\"UpdatedDate\":null,\"ExpireOn\":null,\"Active\":true},{\"$id\":\"2\",\"SurveyId\":2,\"Title\":\"Santa Cruz Survey\",\"Description\":\"This is Santa Cruz Survey\",\"CreatedBy\":1,\"CreatedDate\":\"2013-11-05T12:04:23\",\"UpdatedBy\":null,\"UpdatedDate\":null,\"ExpireOn\":null,\"Active\":true}]";
     public static final String testSurvey = "{\"$id\":\"1\",\"SurveyId\":1,\"Title\":\"Santa Clara Survey\",\"SurveyQuestions\":[{\"$id\":\"2\",\"QuestionId\":1,\"text\":\"What is your birthday?\",\"QuestionType\":\"SinglelineTextBox\",\"Options\":null,\"OrderId\":1},{\"$id\":\"3\",\"QuestionId\":2,\"text\":\"How do you identify yourself?\",\"QuestionType\":\"SingleSelect\",\"Options\":\"Male|Female|Transgender|Other\",\"OrderId\":2},{\"$id\":\"4\",\"QuestionId\":3,\"text\":\"Which racial/ethinic group do you identify yourself with the most?\",\"QuestionType\":\"SingleSelect\",\"Options\":\"White/Caucasian|Black/African American|Hispanic/Latino|American Indian/Alaskan Native|Vietnamese|Other Asian|Pacific Islander|Other/Multi-ethnic\",\"OrderId\":3}]}";
 
-    public static ArrayList<String> parseSurveyList(String surveys)
+    public static ArrayList<SurveyListing> parseSurveyList(String surveys)
     {
+        ArrayList<SurveyListing> surveyListings = new ArrayList<SurveyListing>();
+
         try {
             JSONArray surveysJSON = new JSONArray(surveys);
 
             for (int i = 0; i < surveysJSON.length(); i++)
             {
                 JSONObject single = surveysJSON.getJSONObject(i);
-                Log.d("JSON", single.get("Title").toString());
+                SurveyListing surveyListing = new SurveyListing(single.getInt("SurveyId"), 1, single.getString("Title"));
+                surveyListings.add(surveyListing);
+                Log.d("SURVEY FOUND", surveyListing.getTitle());
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        return null;
+        return surveyListings;
     }
 
     public static JSONArray parseSurvey(String survey) {
         JSONArray questions = null;
+        ArrayList<Question> questionsList = new ArrayList<Question>();
 
         try {
             JSONObject surveyJSON = new JSONObject(survey);
@@ -42,24 +50,52 @@ public class JSONParser {
             questions = surveyJSON.getJSONArray("SurveyQuestions");
 
             for (int i = 0; i < questions.length(); i++) {
-                Log.d("JSON", questions.getJSONObject(i).get("text").toString());
+                Question question = new Question();
 
+                question.setQuestionId(questions.getJSONObject(i).getInt("QuestionId"));
+                question.setText(questions.getJSONObject(i).getString("text"));
+                question.setQuestionType(questions.getJSONObject(i).getString("QuestionType"));
+                question.setOptions(questions.getJSONObject(i).getString("Options"));
+                question.setOrderId(questions.getJSONObject(i).getInt("OrderId"));
 
+                questionsList.add(question);
+
+                //Log.d("JSON", questions.getJSONObject(i).get("text").toString());
             }
-
-            //JSONArray surveysJSON = new JSONArray(survey);
-
-//            for (int i = 0; i < surveysJSON.length(); i++)
-//            {
-//                JSONObject single = surveysJSON.getJSONObject(i);
-//
-//                Log.d("JSON", single.get("Title").toString());
-//            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
         return questions;
+
+    }
+
+    public static ArrayList<Question> parseSurveyQuestions(String survey) {
+        ArrayList<Question> questionsList = new ArrayList<Question>();
+
+        try {
+            JSONObject surveyJSON = new JSONObject(survey);
+
+            JSONArray questions = surveyJSON.getJSONArray("SurveyQuestions");
+
+            for (int i = 0; i < questions.length(); i++) {
+                Question question = new Question();
+
+                question.setQuestionId(questions.getJSONObject(i).getInt("QuestionId"));
+                question.setText(questions.getJSONObject(i).getString("text"));
+                question.setQuestionType(questions.getJSONObject(i).getString("QuestionType"));
+                question.setOptions(questions.getJSONObject(i).getString("Options"));
+                question.setOrderId(questions.getJSONObject(i).getInt("OrderId"));
+
+                questionsList.add(question);
+
+                //Log.d("JSON", questions.getJSONObject(i).get("text").toString());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return questionsList;
 
     }
 
--- housing1000/android/Housing1000/Housing1000/src/main/java/edu/weber/housing1000/SelectPageActivity.java
+++ housing1000/android/Housing1000/Housing1000/src/main/java/edu/weber/housing1000/SelectPageActivity.java
@@ -1,21 +1,58 @@
 package edu.weber.housing1000;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class SelectPageActivity extends Activity {
-    /**Called when the activity is first created. */
-	
-	TextView txt_display;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.weber.housing1000.Helpers.RESTHelper;
+import edu.weber.housing1000.data.SurveyListing;
+import edu.weber.housing1000.db.DatabaseConnector;
+
+public class SelectPageActivity extends Activity implements RESTHelper.OnUrlTaskCompleted {
+    public static final int TASK_GET_SURVEY_LIST = 1;
+    public static final int TASK_GET_SURVEY = 2;
+    public static final String SURVEY = "survey";
+
+    private ProgressDialog progressDialog;
+
+    private ArrayList<SurveyListing> surveyListings;
+    private long chosenSurveyId;
+
+    private CursorAdapter surveyAdapter;
+    private ListView surveyList;
+    private Dialog surveyDialog;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_page);
+
+        // Set up the adapter and ListView for the survey selection
+        //String[] from = new String[] { "_id", "title" };
+        String[] from = new String[] { "title" };
+        int[] to = new int[] { R.id.surveyTextView };
+        surveyAdapter = new SimpleCursorAdapter(SelectPageActivity.this,
+                R.layout.survey_list_item, null, from, to,
+                CursorAdapter.NO_SELECTION);
+        surveyList = new ListView(this);
+        surveyList.setAdapter(surveyAdapter);
+        surveyList.setOnItemClickListener(surveyClickListener);
 
         //Get the Buttons
         Button clientButton = (Button) findViewById(R.id.censusBtn);
@@ -24,8 +61,10 @@ public class SelectPageActivity extends Activity {
 
         dynamicButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(SelectPageActivity.this, ClientInfoActivity_Dynamic.class);
-                startActivity(intent);
+                getSurveyList();
+
+                //Intent intent = new Intent(SelectPageActivity.this, SelectPageActivity.class);
+                //startActivity(intent);
             }
         });
 
@@ -42,12 +81,202 @@ public class SelectPageActivity extends Activity {
                 startActivity(intent);
             }
         });
-        
-      //  txt_display = (TextView) findViewById(R.id.txt_display);
 
-        //txt_display.setText("Hello Youtube");
+    }
 
-      //  setContentView(R.layout.main);
+    private void getSurveyList()
+    {
+        // Start the loading dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setMessage("Downloading survey list...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(true);
+        progressDialog.show();
+
+        // Set up the UrlTask
+        final RESTHelper.UrlTask urlTask = new RESTHelper.UrlTask(this, TASK_GET_SURVEY_LIST);
+
+        HashMap<String, String> properties = new HashMap<String, String>();
+        properties.put(RESTHelper.ACTION_TYPE, RESTHelper.GET);
+        properties.put(RESTHelper.URL, "http://staging.ctagroup.org/survey/api/survey/");
+
+        urlTask.execute(properties);
+
+        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                urlTask.cancel(true);
+            }
+        });
+    }
+
+    /**
+     * Called when a RESTHelper.UrlTask is completed
+     * @param result Result from the task
+     */
+    @Override
+    public void onUrlTaskCompleted(HashMap<String, String> result) {
+        String taskResult = result.get(RESTHelper.RESULT);
+
+        switch(Integer.parseInt(result.get(RESTHelper.TASK_CODE)))
+        {
+            case TASK_GET_SURVEY_LIST:
+                if (taskResult.length() > 0 && !taskResult.startsWith("ERROR: "))
+                {
+                    surveyListings = JSONParser.parseSurveyList(taskResult);
+                    if (surveyListings.size() == 0)
+                    {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                        builder.setTitle("No Surveys...");
+                        builder.setMessage("It appears that there are no surveys yet.");
+                        builder.show();
+                    }
+                    else
+                    {
+                        Toast.makeText(SelectPageActivity.this, surveyListings.size() + " surveys found.", Toast.LENGTH_SHORT).show();
+
+                        DatabaseConnector dbConnector = new DatabaseConnector(this);
+                        dbConnector.clearSurveys();
+
+                        for (SurveyListing survey : surveyListings)
+                        {
+                            dbConnector.insertSurvey(survey);
+                        }
+
+                        new GetAllSurveysTask().execute((Object[]) null);
+                    }
+                }
+                else
+                {
+                    progressDialog.dismiss();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Uh oh...");
+                    builder.setMessage("There was a problem downloading the surveys.\n\nMessage:\n" + taskResult);
+                    builder.show();
+                }
+                break;
+            case TASK_GET_SURVEY:
+
+                for (SurveyListing survey : surveyListings)
+                {
+                    if (survey.getId() == chosenSurveyId)
+                    {
+                        DatabaseConnector dbConnector = new DatabaseConnector(this);
+
+                        survey.setQuestions(taskResult);
+                        dbConnector.updateSurvey(survey);
+
+                        progressDialog.dismiss();
+
+                        launchSurveyActivity(survey);
+
+                        break;
+                    }
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Displays a dialog with the list of surveys
+     */
+    private void displaySurveyList()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Survey");
+        builder.setView(surveyList);
+        surveyDialog = builder.create();
+
+        surveyDialog.show();
+    }
+
+    /**
+     * Performs a database query outside of the GUI thread
+     */
+    private class GetAllSurveysTask extends AsyncTask<Object, Object, Cursor> {
+        DatabaseConnector databaseConnector = new DatabaseConnector(
+                SelectPageActivity.this);
+
+        // perform the database access
+        @Override
+        protected Cursor doInBackground(Object... params) {
+            databaseConnector.open();
+
+            // get a cursor containing all surveys
+            return databaseConnector.getAllSurveys();
+        } // end method doInBackground
+
+        // use the Cursor returned from the doInBackground method
+        @Override
+        protected void onPostExecute(Cursor result) {
+            surveyAdapter.changeCursor(result); // set the adapter's Cursor
+            databaseConnector.close();
+
+            progressDialog.dismiss();
+
+            displaySurveyList();
+        } // end method onPostExecute
+    } // end class GetContactsTask
+
+    /**
+     * Handles the survey click events
+     */
+    AdapterView.OnItemClickListener surveyClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                long arg3) {
+
+            chosenSurveyId = arg3;
+
+            surveyDialog.dismiss();
+
+            loadSurvey(chosenSurveyId);
+
+        } // end method onItemClick
+    }; // end viewContactListener
+
+    private void loadSurvey(long rowId)
+    {
+        // Start the loading dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setMessage("Downloading survey...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setCancelable(true);
+        progressDialog.show();
+
+        // Set up the UrlTask
+        final RESTHelper.UrlTask urlTask = new RESTHelper.UrlTask(this, TASK_GET_SURVEY);
+
+        HashMap<String, String> properties = new HashMap<String, String>();
+        properties.put(RESTHelper.ACTION_TYPE, RESTHelper.GET);
+        properties.put(RESTHelper.URL, "http://staging.ctagroup.org/survey/api/survey/" + String.valueOf(rowId));
+
+        urlTask.execute(properties);
+
+        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                urlTask.cancel(true);
+            }
+        });
+    }
+
+    private void launchSurveyActivity(SurveyListing survey)
+    {
+        Intent launchSurvey = new Intent(SelectPageActivity.this,
+                ClientInfoActivity_Dynamic.class);
+
+        // Pass the selected survey's row ID as an extra with the Intent
+        launchSurvey.putExtra(SURVEY, survey);
+        startActivity(launchSurvey); // start the ViewContact Activity
     }
 
 }
