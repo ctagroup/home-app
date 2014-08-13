743dc21af1e4fbd249e66e5e8e2eebccc859a6fe
davidhorton <student@Jamess-Mac-mini.local>
2014-08-13 17:36:52 -0600

    Added GPS class to iOS

---
+++ housing1000/Housing1000_iOS/Housing1000/GPSLocationRetriever.h
@@ -0,0 +1,13 @@
+//
+//  GPSLocationRetriever.h
+//  Housing1000
+//
+//  Created by student on 8/13/14.
+//  Copyright (c) 2014 Group 3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GPSLocationRetriever : NSObject
+
+@end
---
+++ housing1000/Housing1000_iOS/Housing1000/GPSLocationRetriever.m
@@ -0,0 +1,13 @@
+//
+//  GPSLocationRetriever.m
+//  Housing1000
+//
+//  Created by student on 8/13/14.
+//  Copyright (c) 2014 Group 3. All rights reserved.
+//
+
+#import "GPSLocationRetriever.h"
+
+@implementation GPSLocationRetriever
+
+@end
