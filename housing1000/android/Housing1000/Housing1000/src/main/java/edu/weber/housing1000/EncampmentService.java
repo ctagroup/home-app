f94378754b6e43baa1d29547fd1d372379b0f3ab
davidhorton <student@Jamess-Mac-mini.local>
2014-08-13 01:42:48 -0600

    New encampment Retrofit service. Figured it made more sense to keep it separate from the survey service.

---
+++ housing1000/android/Housing1000/Housing1000/src/main/java/edu/weber/housing1000/EncampmentService.java
@@ -0,0 +1,31 @@
+package edu.weber.housing1000;
+
+import java.util.ArrayList;
+
+import edu.weber.housing1000.data.EncampmentSite;
+import edu.weber.housing1000.data.Survey;
+import edu.weber.housing1000.data.SurveyResponse;
+import retrofit.Callback;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.Query;
+
+/**
+ *
+ * @author David Horton
+ *
+ * This is used to perform our REST API calls
+ * More info here: http://square.github.io/retrofit/
+ */
+public interface EncampmentService {
+
+    @GET("/Outreach/api/EncampmentSite")
+    void searchEncampment(@Query("searchStr") String searchString, Callback<ArrayList<EncampmentSite>> cb);
+
+    @GET("/Outreach/api/EncampmentSite")
+    void getEncampmentQuestions(Callback<String> cb);
+
+    @POST("/Outreach/api/EncampmentSite")
+    void postEncampmentResponse(@Body SurveyResponse surveyResponse, Callback<String> cb);
+}
