559ecccc01c366f47dba0b71d5843ae812918f7b
Udit Desai <desaiuditd@gmail.com>
2015-12-12 17:22:30 -0800

    adds app & admin dashboard routes

--- router/app.js
+++ router/app.js
@@ -9,48 +9,103 @@
 /**
  * Home Routes
  */
-Router.route( '/', function () {
-	this.render( 'home' );
-});
-Router.route( '/home', function() {
-	this.render( 'home' );
+Router.route( '/', {
+	name: 'root',
+	template: 'home',
+} );
+Router.route( '/home', {
+	name: 'home',
+	template: 'home',
 } );
 
 /**
  * Accounts Routes
  */
 AccountsTemplates.configureRoute( 'changePwd', {
+	name: 'changePwd',
 	path: '/change-password',
 	template: 'login'
 } );
 AccountsTemplates.configureRoute( 'enrollAccount', {
+	name: 'enrollAccount',
 	path: '/enroll-account',
 	template: 'login'
 } );
 AccountsTemplates.configureRoute( 'forgotPwd', {
+	name: 'forgotPwd',
 	path: '/forgot-password',
 	template: 'login'
 } );
 AccountsTemplates.configureRoute( 'resetPwd', {
+	name: 'resetPwd',
 	path: '/reset-password',
 	template: 'login'
 } );
 AccountsTemplates.configureRoute( 'signIn', {
+	name: 'signIn',
 	path: '/login',
-	template: 'login'
+	template: 'login',
+	redirect: '/app'
 } );
 AccountsTemplates.configureRoute( 'signUp', {
+	name: 'signUp',
 	path: '/register',
 	template: 'login'
 } );
 AccountsTemplates.configureRoute( 'verifyEmail', {
+	name: 'verifyEmail',
 	path: '/verify-email',
 	template: 'login'
 } );
 AccountsTemplates.configureRoute( 'resendVerificationEmail', {
+	name: 'resendVerificationEmail',
 	path: '/send-again',
 	template: 'login'
-} )
+} );
+
+/**
+ * App Routes
+ */
+Router.route( '/app', {
+	name: 'appRoot',
+	template: 'appDashboard'
+});
+Router.route( '/app/dashboard', {
+	name: 'appDashboard',
+	template: 'appDashboard'
+});
+
+/**
+ * Admin Routes
+ */
+Router.route( '/admin', {
+	name: 'adminRoot',
+	template: 'adminDashboard'
+} );
+Router.route( '/admin/dashboard', {
+	name: 'adminDashboard',
+	template: 'adminDashboard'
+} );
+
+/**
+ * Ensure User Login for templates
+ */
+Router.plugin( 'ensureSignedIn', {
+	except: [
+		'root',
+		'home',
+		'changePwd',
+		'enrollAccount',
+		'forgotPwd',
+		'resetPwd',
+		'signIn',
+		'signUp',
+		'verifyEmail',
+		'resendVerificationEmail',
+		'privacy',
+		'terms-of-use'
+	]
+} );
 
 /**
  * Misc Routes
