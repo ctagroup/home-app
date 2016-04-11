dc5a3d08f686650bceaa0f9cbee8864cce43fb30
anush24 <agovindan89@gmail.com>
2016-04-11 13:57:50 -0700

    Client Profile-Database Fields Integration

--- client/controllers/common/clientInfoHelpers.js
+++ client/controllers/common/clientInfoHelpers.js
@@ -1,6 +1,7 @@
 /**
  * Created by Kavi on 4/9/16.
  */
+var gender_categories,veteran_status,ethnicity_categories,race_categories,residence_prior,destination_category,relationship_categories,times_Homeless,disabling_cond;
 Template.clientProfile.helpers(
     {
         clientInfoList: function() {
@@ -10,9 +11,92 @@ Template.clientProfile.helpers(
         viewClientPath: function(id) {
             var viewpath = Router.path( "adminDashboard" + Session.get('admin_collection_name') + "View", {_id: id} );
             return viewpath;
-        }
-    }
-    
+        },
+		getUniversalElements:function () {
+			getUniversalElements();
+		},
+		getGender:function () {
+			return gender_categories;
+		},
+		getVeteranStatus:function () {
+			return veteran_status;
+		},
+		getRace:function () {
+			return race_categories;
+		},
+		getEthnicity:function () {
+			return ethnicity_categories;
+		},
+		getResidencePrior:function () {
+			return residence_prior;
+		},
+		getDestination:function () {
+			return destination_category;
+		},
+		getRelationshipToHoh:function () {
+			return relationship_categories;
+		},
+		getTimesHomeless:function () {
+			return times_Homeless;
+		},
+		getDisablingCondition:function () {
+			return disabling_cond;
+		}
+	}
+
 );
+var getUniversalElements=function(){
+	var questionsCollection = adminCollectionObject("questions");
+	var universalElements= questionsCollection.find({'category':'Universal Data Elements'}).fetch();
+	for(var i in universalElements){
+		var q_name =universalElements[i].name;
+		if(q_name=="gender")
+		{
+			var genderCategories=universalElements[i].options;
+			console.log("gender"+genderCategories);
+			gender_categories=genderCategories.split('|');
+		}else if(q_name=="veteran_status")
+		{
+			var veteranStatus=universalElements[i].options;
+			veteran_status=veteranStatus.split('|');
+		}else if(q_name=="ethnicity")
+		{
+			var ethnicity=universalElements[i].options;
+			console.log("ethnicity"+ethnicity);
+			ethnicity_categories=ethnicity.split('|');
+		}
+		else if(q_name=="race")
+		{
+			var race=universalElements[i].options;
+			race_categories=race.split('|');
+		}
+		else if(q_name=="residence_prior_to_entry")
+		{
+			var residenceprior=universalElements[i].options;
+			residence_prior=residenceprior.split('|');
+		}else if(q_name=="destination")
+		{
+			var destination=universalElements[i].options;
+			destination_category=destination.split('|');
+		}else if(q_name=="relationshiptohoh")
+		{
+			var relationship=universalElements[i].options;
+			relationship_categories=relationship.split('|');
+		}
+		else if(q_name=="timesHomelesspastthreeyears")
+		{
+			var timesHomeless=universalElements[i].options;
+			times_Homeless=timesHomeless.split('|');
+		}
+		else if(q_name=="disabling_cond")
+		{
+			var disablingcondition=universalElements[i].options;
+			disabling_cond=disablingcondition.split('|');
+		}
+	}
+
+};
+
+
 
 
