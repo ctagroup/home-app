b1b32b95d098bf13a62c614d1a6db2a795c0170b
Udit Desai <desaiuditd@gmail.com>
2016-06-15 09:39:04 -0700

    removes templates/routes for password strategy

--- config/accounts.js
+++ config/accounts.js
@@ -3,12 +3,6 @@
  */
 AccountsTemplates.configure({
 
-	enablePasswordChange: true,
-	showForgotPasswordLink: true,
-    enforceEmailVerification: true,
-    sendVerificationEmail: true,
-	showResendVerificationEmailLink: true,
-
 	// Privacy Policy and Terms of Use
 	privacyUrl: 'privacy',
 	termsUrl: 'terms-of-use',
