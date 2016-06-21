6facb7debb8085341fffe0cf11b308415d226f54
Udit Desai <desaiuditd@gmail.com>
2016-06-21 14:54:08 -0700

    fixes eslint errors in accounts.js

--- server/config/accounts.js
+++ server/config/accounts.js
@@ -3,19 +3,21 @@
  */
 
 // Set up login services
-Meteor.startup(function() {
-	// Add HMIS configuration entry
-	ServiceConfiguration.configurations.remove({service:"google"});
-	ServiceConfiguration.configurations.remove({service:"facebook"});
-	ServiceConfiguration.configurations.update( {
-		service: "HMIS",
-	}, {
-		$set: {
-			hmisAPIEndpoints: AdminConfig.hmisAPIEndpoints,
-			appId: "16631CFE-6909-4AC1-B4EB-57902AC7AF0A",
-			appSecret: "appSecret"
-		}
-	}, {
-		upsert: true
-	} );
-} );
+Meteor.startup(
+  () => {
+    // Add HMIS configuration entry
+    ServiceConfiguration.configurations.update(
+      {
+        service: 'HMIS',
+      }, {
+        $set: {
+          hmisAPIEndpoints: AdminConfig.hmisAPIEndpoints,
+          appId: '16631CFE-6909-4AC1-B4EB-57902AC7AF0A',
+          appSecret: 'appSecret',
+        },
+      }, {
+        upsert: true,
+      }
+    );
+  }
+);
