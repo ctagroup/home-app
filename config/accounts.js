ae2efd833b78ab59094d21532a4197ca265b5ba1
Udit Desai <desaiuditd@gmail.com>
2016-06-22 16:45:56 -0700

    fixes ESLint issues for config folder

--- config/accounts.js
+++ config/accounts.js
@@ -1,9 +1,10 @@
 /**
  * Created by udit on 12/12/15.
  */
-AccountsTemplates.configure({
-
-	// Privacy Policy and Terms of Use
-	privacyUrl: 'privacy',
-	termsUrl: 'terms-of-use',
-} );
+AccountsTemplates.configure(
+  {
+    // Privacy Policy and Terms of Use
+    privacyUrl: 'privacy',
+    termsUrl: 'terms-of-use',
+  }
+);
--- config/adminConfig.js
+++ config/adminConfig.js
@@ -3,294 +3,300 @@
  */
 
 AdminConfig = {
-	name: 'H.O.M.E.',
-	adminEmails: [
-		'desaiuditd@gmail.com',
-	    'kavithamuthu14@gmail.com',
-	    'anushagovindan@gmail.com',
-	    'javier@ctagroup.org',
-		'bob@ctagroup.org',
-	    'kgautam@scu.edu',
-		'kghate@scu.edu',
-		'agovindan@scu.edu'
-	],
-	defaultHomeRoles: [
-		"Developer",
-	    "System Admin",
-	    "Admin",
-	    "Program Manager",
-	    "Case Manager",
-	    "Surveyor"
-	],
-	defaultPermissions: [
-		"Developer",
-		"System Admin",
-		"Admin",
-		"Program Manager",
-		"Case Manager",
-		"Surveyor",
-		"view_admin",
-		"view_organization",
-		"create_organization",
-		"edit_organization",
-		"delete_organization",
-		"update_referral",
-		"view_user",
-		"create_user",
-		"edit_user",
-		"delete_user",
-		"manage_user_role",
-		"manage_user_status",
-		"reset_user_password",
-		"view_reports",
-		"view_survey",
-		"create_survey",
-		"edit_survey",
-		"delete_survey",
-		"view_project",
-		"create_project",
-		"edit_project",
-		"delete_project",
-		"view_inventory",
-		"create_inventory",
-		"edit_inventory",
-		"delete_inventory",
-		"view_intake",
-		"create_intake",
-		"edit_intake",
-		"delete_intake",
-		"manage_settings"
-	],
-	defaultRolePermissions: {
-		"Developer": [
-			"Developer",
-			"System Admin",
-			"Admin",
-			"Program Manager",
-			"Case Manager",
-			"Surveyor",
-			"view_admin",
-			"view_organization",
-			"create_organization",
-			"edit_organization",
-			"delete_organization",
-			"update_referral",
-			"view_user",
-			"create_user",
-			"edit_user",
-			"delete_user",
-			"manage_user_role",
-			"manage_user_status",
-			"reset_user_password",
-			"view_reports",
-			"view_survey",
-			"create_survey",
-			"edit_survey",
-			"delete_survey",
-			"view_project",
-			"create_project",
-			"edit_project",
-			"delete_project",
-			"view_inventory",
-			"create_inventory",
-			"edit_inventory",
-			"delete_inventory",
-			"view_intake",
-			"create_intake",
-			"edit_intake",
-			"delete_intake",
-			"manage_settings"
-		],
-		"System Admin": [
-			"System Admin",
-			"Admin",
-			"Program Manager",
-			"Case Manager",
-			"Surveyor",
-			"view_admin",
-			"view_organization",
-			"create_organization",
-			"edit_organization",
-			"delete_organization",
-			"update_referral",
-			"view_user",
-			"create_user",
-			"edit_user",
-			"delete_user",
-			"manage_user_role",
-			"manage_user_status",
-			"reset_user_password",
-			"view_reports",
-			"view_survey",
-			"create_survey",
-			"edit_survey",
-			"delete_survey",
-			"view_project",
-			"create_project",
-			"edit_project",
-			"delete_project",
-			"view_inventory",
-			"create_inventory",
-			"edit_inventory",
-			"delete_inventory",
-			"view_intake",
-			"create_intake",
-			"edit_intake",
-			"delete_intake",
-			"manage_settings"
-		],
-		"Admin": [
-			"Admin",
-			"Program Manager",
-			"Case Manager",
-			"Surveyor",
-			"view_admin",
-			"view_user",
-			"create_user",
-			"edit_user",
-			"delete_user",
-			"reset_user_password",
-			"view_reports",
-			"view_project",
-			"create_project",
-			"edit_project",
-			"delete_project",
-			"view_inventory",
-			"create_inventory",
-			"edit_inventory",
-			"delete_inventory"
-		],
-		"Program Manager": [
-			"Program Manager",
-			"Case Manager",
-			"Surveyor",
-			"view_admin",
-			"view_user",
-			"view_reports",
-			"view_inventory",
-			"edit_inventory",
-		],
-		"Case Manager": [
-			"Case Manager",
-			"Surveyor",
-			"view_admin",
-			"update_referral",
-			"view_inventory",
-			"view_intake",
-			"create_intake",
-			"edit_intake",
-			"delete_intake",
-		],
-		"Surveyor": [
-			"Surveyor",
-			"view_admin",
-			"view_survey",
-			"view_intake",
-			"create_intake",
-			"edit_intake",
-			"delete_intake",
-		]
-	},
-	collections: {
-		surveys: {
-			icon: 'file-text',
-			label: 'Surveys',
-			tableColumns: [
-				{
-					name: 'title',
-					label: 'Title',
-					render: function( value, type, doc ) {
-						var path = Router.path( "adminDashboard" + Session.get('admin_collection_name') + "Edit", {_id: doc._id} );
-						return '<a href="' + path + '"><strong>' + value + '</strong></a>';
-					}
-				}
-			],
-			templates: {
-				view : {
-					name: 'surveyViewTemplate'
-				},
-				edit: {
-					name: 'surveyEditTemplate',
-					data: function () {
-						return Meteor.isClient && Session.get('admin_doc');
-					}
-				}
-			}
-		},
-		questions: {
-			icon: 'question',
-			label: 'Questions',
-			templates: {
-				view: {
-					name: 'questionViewTemplate'
-				}
-			}
-		},
-		users: {
-			icon: 'user',
-			label: 'Users',
-			addNewLink: true,
-			templates: {
-				edit: {
-					name: 'AdminDashboardusersEdit'
-				}
-			},
-			changeSelector: function( selector, userId ) {
-				$or = selector['$or'];
-				if ( $or ) {
-					selector['$or'] = _.map( $or, function ( exp ) {
-						var ref;
-						if ( ( ( ref = exp.emails ) != null ? ref['$regex'] : void 0 ) != null ) {
-							return {
-								emails: {
-									$elemMatch: {
-										address: exp.emails
-									}
-								}
-							};
-						} else {
-							return exp;
-						}
-					} );
-				}
-				return selector;
-			},
-			tableColumns: [
-				{
-					name: 'emails',
-					label: 'Email',
-					render: function( value ) {
-						//some users have no email addresses
-						if ( value && value.length ) {
-							return value[0].address;
-						}
-						return '';
-					},
-					searchable: true
-				},
-				{
-					name: 'emails',
-					label: 'Mail',
-					template: 'adminUsersMailBtn',
-					width: '40px'
-				},
-				{
-					name: 'createdAt',
-					label: 'Joined'
-				}
-			]
-		}
-	},
-	hmisAPIEndpoints: {
-		oauthBaseUrl: "https://www.hmislynk.com/hmis-authorization-service/rest",
-		authorize: "/authorize/",
-		token: "/token/",
-		revoke: "/revoke/",
-		clientBaseUrl: "https://www.hmislynk.com/hmis-clientapi/rest",
-		clients: "/clients/",
-		userServiceBaseUrl: "http://www.hmislynk.com/hmis-user-service/rest",
-		basicInfo: "/accounts/{{username}}/basicinfo",
-		selfBasicInfo: "/accounts/self/basicinfo",
-		searchClient: "/search/client"
-	}
+  name: 'H.O.M.E.',
+  adminEmails: [
+    'desaiuditd@gmail.com',
+    'kavithamuthu14@gmail.com',
+    'anushagovindan@gmail.com',
+    'javier@ctagroup.org',
+    'bob@ctagroup.org',
+    'kgautam@scu.edu',
+    'kghate@scu.edu',
+    'agovindan@scu.edu',
+  ],
+  defaultHomeRoles: [
+    'Developer',
+    'System Admin',
+    'Admin',
+    'Program Manager',
+    'Case Manager',
+    'Surveyor',
+  ],
+  defaultPermissions: [
+    'Developer',
+    'System Admin',
+    'Admin',
+    'Program Manager',
+    'Case Manager',
+    'Surveyor',
+    'view_admin',
+    'view_organization',
+    'create_organization',
+    'edit_organization',
+    'delete_organization',
+    'update_referral',
+    'view_user',
+    'create_user',
+    'edit_user',
+    'delete_user',
+    'manage_user_role',
+    'manage_user_status',
+    'reset_user_password',
+    'view_reports',
+    'view_survey',
+    'create_survey',
+    'edit_survey',
+    'delete_survey',
+    'view_project',
+    'create_project',
+    'edit_project',
+    'delete_project',
+    'view_inventory',
+    'create_inventory',
+    'edit_inventory',
+    'delete_inventory',
+    'view_intake',
+    'create_intake',
+    'edit_intake',
+    'delete_intake',
+    'manage_settings',
+  ],
+  defaultRolePermissions: {
+    Developer: [
+      'Developer',
+      'System Admin',
+      'Admin',
+      'Program Manager',
+      'Case Manager',
+      'Surveyor',
+      'view_admin',
+      'view_organization',
+      'create_organization',
+      'edit_organization',
+      'delete_organization',
+      'update_referral',
+      'view_user',
+      'create_user',
+      'edit_user',
+      'delete_user',
+      'manage_user_role',
+      'manage_user_status',
+      'reset_user_password',
+      'view_reports',
+      'view_survey',
+      'create_survey',
+      'edit_survey',
+      'delete_survey',
+      'view_project',
+      'create_project',
+      'edit_project',
+      'delete_project',
+      'view_inventory',
+      'create_inventory',
+      'edit_inventory',
+      'delete_inventory',
+      'view_intake',
+      'create_intake',
+      'edit_intake',
+      'delete_intake',
+      'manage_settings',
+    ],
+    'System Admin': [
+      'System Admin',
+      'Admin',
+      'Program Manager',
+      'Case Manager',
+      'Surveyor',
+      'view_admin',
+      'view_organization',
+      'create_organization',
+      'edit_organization',
+      'delete_organization',
+      'update_referral',
+      'view_user',
+      'create_user',
+      'edit_user',
+      'delete_user',
+      'manage_user_role',
+      'manage_user_status',
+      'reset_user_password',
+      'view_reports',
+      'view_survey',
+      'create_survey',
+      'edit_survey',
+      'delete_survey',
+      'view_project',
+      'create_project',
+      'edit_project',
+      'delete_project',
+      'view_inventory',
+      'create_inventory',
+      'edit_inventory',
+      'delete_inventory',
+      'view_intake',
+      'create_intake',
+      'edit_intake',
+      'delete_intake',
+      'manage_settings',
+    ],
+    Admin: [
+      'Admin',
+      'Program Manager',
+      'Case Manager',
+      'Surveyor',
+      'view_admin',
+      'view_user',
+      'create_user',
+      'edit_user',
+      'delete_user',
+      'reset_user_password',
+      'view_reports',
+      'view_project',
+      'create_project',
+      'edit_project',
+      'delete_project',
+      'view_inventory',
+      'create_inventory',
+      'edit_inventory',
+      'delete_inventory',
+    ],
+    'Program Manager': [
+      'Program Manager',
+      'Case Manager',
+      'Surveyor',
+      'view_admin',
+      'view_user',
+      'view_reports',
+      'view_inventory',
+      'edit_inventory',
+    ],
+    'Case Manager': [
+      'Case Manager',
+      'Surveyor',
+      'view_admin',
+      'update_referral',
+      'view_inventory',
+      'view_intake',
+      'create_intake',
+      'edit_intake',
+      'delete_intake',
+    ],
+    Surveyor: [
+      'Surveyor',
+      'view_admin',
+      'view_survey',
+      'view_intake',
+      'create_intake',
+      'edit_intake',
+      'delete_intake',
+    ],
+  },
+  collections: {
+    surveys: {
+      icon: 'file-text',
+      label: 'Surveys',
+      tableColumns: [
+        {
+          name: 'title',
+          label: 'Title',
+          render(value, type, doc) {
+            const path = Router.path(
+              `adminDashboard${Session.get('admin_collection_name')}Edit`,
+              { _id: doc._id }
+            );
+            return `<a href="${path}"><strong>${value}</strong></a>`;
+          },
+        },
+      ],
+      templates: {
+        view: {
+          name: 'surveyViewTemplate',
+        },
+        edit: {
+          name: 'surveyEditTemplate',
+          data() {
+            return Meteor.isClient && Session.get('admin_doc');
+          },
+        },
+      },
+    },
+    questions: {
+      icon: 'question',
+      label: 'Questions',
+      templates: {
+        view: {
+          name: 'questionViewTemplate',
+        },
+      },
+    },
+    users: {
+      icon: 'user',
+      label: 'Users',
+      addNewLink: true,
+      templates: {
+        edit: {
+          name: 'AdminDashboardusersEdit',
+        },
+      },
+      changeSelector(selector) {
+        const selectorz = selector;
+        const $or = selectorz.$or;
+        if ($or) {
+          selectorz.$or = _.map(
+            $or, (exp) => {
+              let expz = exp;
+              const ref = expz.emails;
+              if ((ref != null ? ref.$regex : void 0) != null) {
+                expz = {
+                  emails: {
+                    $elemMatch: {
+                      address: expz.emails,
+                    },
+                  },
+                };
+              }
+              return expz;
+            }
+          );
+        }
+        return selectorz;
+      },
+      tableColumns: [
+        {
+          name: 'emails',
+          label: 'Email',
+          render(value) {
+            // some users have no email addresses
+            if (value && value.length) {
+              return value[0].address;
+            }
+            return '';
+          },
+          searchable: true,
+        },
+        {
+          name: 'emails',
+          label: 'Mail',
+          template: 'adminUsersMailBtn',
+          width: '40px',
+        },
+        {
+          name: 'createdAt',
+          label: 'Joined',
+        },
+      ],
+    },
+  },
+  hmisAPIEndpoints: {
+    oauthBaseUrl: 'https://www.hmislynk.com/hmis-authorization-service/rest',
+    authorize: '/authorize/',
+    token: '/token/',
+    revoke: '/revoke/',
+    clientBaseUrl: 'https://www.hmislynk.com/hmis-clientapi/rest',
+    clients: '/clients/',
+    userServiceBaseUrl: 'http://www.hmislynk.com/hmis-user-service/rest',
+    basicInfo: '/accounts/{{username}}/basicinfo',
+    selfBasicInfo: '/accounts/self/basicinfo',
+    searchClient: '/search/client',
+  },
 };
