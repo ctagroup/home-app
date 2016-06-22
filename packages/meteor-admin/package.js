8dd71e036599546b6f76ca13fda81ed17ffe000d
Udit Desai <desaiuditd@gmail.com>
2016-06-22 15:00:22 -0700

    fixes eslint issues for admin package

--- packages/meteor-admin/package.js
+++ packages/meteor-admin/package.js
@@ -1,79 +1,90 @@
-Package.describe({
-  name: "desaiuditd:meteor-admin",
-  summary: "A complete admin dashboard solution",
-  version: "1.2.3",
-  git: "https://github.com/desaiuditd/meteor-admin"
-});
+Package.describe(
+  {
+    name: 'desaiuditd:meteor-admin',
+    summary: 'A complete admin dashboard solution',
+    version: '1.2.3',
+    git: 'https://github.com/desaiuditd/meteor-admin',
+  }
+);
 
-Package.on_use(function(api){
+Package.on_use(
+  /* eslint-disable */
+  function meteorAdminOnUse (api) {
+  /* eslint-enable */
+    const both = ['client', 'server'];
 
-  both = ['client','server']
+    api.versionsFrom('METEOR@1.0');
 
-  api.versionsFrom('METEOR@1.0');
+    api.use(
+      [
+        'iron:router@1.0.9',
+        'coffeescript',
+        'check',
+        'underscore',
+        'reactive-var',
+        'aldeed:collection2@2.5.0',
+        'aldeed:autoform@5.5.1',
+        'aldeed:template-extension@3.4.3',
+        'alanning:roles@1.2.13',
+        'raix:handlebar-helpers@0.2.5',
+        'reywood:publish-composite@1.4.2',
+        'momentjs:moment@2.10.6',
+        'aldeed:tabular@1.4.0',
+        'meteorhacks:unblock@1.1.0',
+        'zimme:active-route@2.3.2',
+        'mfactory:admin-lte@0.0.2',
+      ],
+      both
+    );
 
-  api.use(
-    ['iron:router@1.0.9',
-    'coffeescript',
-    'check',
-    'underscore',
-    'reactive-var',
-    'aldeed:collection2@2.5.0',
-    'aldeed:autoform@5.5.1',
-    'aldeed:template-extension@3.4.3',
-    'alanning:roles@1.2.13',
-    'raix:handlebar-helpers@0.2.5',
-    'reywood:publish-composite@1.4.2',
-    'momentjs:moment@2.10.6',
-    'aldeed:tabular@1.4.0',
-    'meteorhacks:unblock@1.1.0',
-    'zimme:active-route@2.3.2',
-    'mfactory:admin-lte@0.0.2'
-    ],
-    both);
+    api.use(['fourseven:scss@3.4.1', 'session', 'jquery', 'templating'], 'client');
 
-  api.use(['fourseven:scss@3.4.1','session','jquery','templating'],'client')
+    api.use(['email'], 'server');
 
-  api.use(['email'],'server')
+    api.add_files(
+      [
+        'lib/both/AdminDashboard.coffee',
+        'lib/both/router.coffee',
+        'lib/both/utils.coffee',
+        'lib/both/startup.coffee',
+        'lib/both/collections.coffee',
+        'lib/both/homeRouter.js',
+        'lib/both/homeStartup.js',
+      ], both
+    );
 
-  api.add_files([
-    'lib/both/AdminDashboard.coffee',
-    'lib/both/router.coffee',
-    'lib/both/utils.coffee',
-    'lib/both/startup.coffee',
-    'lib/both/collections.coffee',
-    'lib/both/homeRouter.js',
-    'lib/both/homeStartup.js'
-    ], both);
+    api.add_files(
+      [
+        'lib/client/html/admin_templates.html',
+        'lib/client/html/survey_templates.html',
+        'lib/client/html/question_templates.html',
+        'lib/client/html/admin_widgets.html',
+        'lib/client/html/admin_layouts.html',
+        'lib/client/html/admin_sidebar.html',
+        'lib/client/html/admin_header.html',
+        'lib/client/stylesheets/style.scss',
+        'lib/client/stylesheets/_admin.scss',
+        'lib/client/stylesheets/_settings.scss',
+        'lib/client/stylesheets/_survey.scss',
+        'lib/client/js/admin_layout.js',
+        'lib/client/js/helpers.coffee',
+        'lib/client/js/homeEvents.js',
+        'lib/client/js/homeHelpers.js',
+        'lib/client/js/homeAutoForm.js',
+        'lib/client/js/templates.coffee',
+        'lib/client/js/events.coffee',
+        'lib/client/js/slim_scroll.js',
+        'lib/client/js/autoForm.coffee',
+      ], 'client'
+    );
 
-  api.add_files([
-    'lib/client/html/admin_templates.html',
-    'lib/client/html/survey_templates.html',
-    'lib/client/html/question_templates.html',
-    'lib/client/html/admin_widgets.html',
-    'lib/client/html/admin_layouts.html',
-    'lib/client/html/admin_sidebar.html',
-    'lib/client/html/admin_header.html',
-	'lib/client/stylesheets/style.scss',
-	'lib/client/stylesheets/_admin.scss',
-	'lib/client/stylesheets/_settings.scss',
-	'lib/client/stylesheets/_survey.scss',
-    'lib/client/js/admin_layout.js',
-    'lib/client/js/helpers.coffee',
-    'lib/client/js/homeEvents.js',
-    'lib/client/js/homeHelpers.js',
-    'lib/client/js/homeAutoForm.js',
-    'lib/client/js/templates.coffee',
-    'lib/client/js/events.coffee',
-    'lib/client/js/slim_scroll.js',
-    'lib/client/js/autoForm.coffee'
-    ], 'client');
+    api.add_files(
+      [
+        'lib/server/publish.coffee',
+        'lib/server/methods.coffee',
+      ], 'server'
+    );
 
-  api.add_files([
-    'lib/server/publish.coffee',
-    'lib/server/methods.coffee',
-    'lib/server/homeMethods.js',
-    'lib/server/homePublish.js'
-    ], 'server');
-
-  api.export('AdminDashboard',both)
-});
+    api.export('AdminDashboard', both);
+  }
+);
---
+++ server/methods/admin.js
@@ -0,0 +1,476 @@
+/**
+ * Created by udit on 05/02/16.
+ */
+Meteor.methods(
+  {
+    getAllHomeRoles() {
+      const homeRolesCollection = adminCollectionObject('homeRoles');
+      return homeRolesCollection.find({}).fetch();
+    },
+    insertHomeRole(data) {
+      const homeRolesCollection = adminCollectionObject('homeRoles');
+      homeRolesCollection.insert(data);
+    },
+    generateDefaultHomeRoles() {
+      const defaultHomeRoles = AdminConfig.defaultHomeRoles;
+      for (let i = 0; i < defaultHomeRoles.length; i++) {
+        Meteor.call('insertHomeRole', { title: defaultHomeRoles[i] });
+      }
+    },
+    getAllHomePermissions() {
+      return Roles.getAllRoles().fetch();
+    },
+    generateHomePermissions() {
+      const defaultPermissions = AdminConfig.defaultPermissions;
+      for (let i = 0; i < defaultPermissions.length; i++) {
+        Roles.createRole(defaultPermissions[i]);
+      }
+    },
+    getAllRolePermissions() {
+      const rolePermissions = adminCollectionObject('rolePermissions');
+      return rolePermissions.find({}).fetch();
+    },
+    generateHomeRolePermissions() {
+      const defaultHomeRoles = AdminConfig.defaultHomeRoles;
+      for (let i = 0; i < defaultHomeRoles.length; i++) {
+        if (AdminConfig && AdminConfig.defaultRolePermissions
+            && AdminConfig.defaultRolePermissions[defaultHomeRoles[i]]) {
+          const rolePermissions = AdminConfig.defaultRolePermissions[defaultHomeRoles[i]];
+          for (let j = 0; j < rolePermissions.length; j++) {
+            const rolePermissionsCollection = adminCollectionObject('rolePermissions');
+            rolePermissionsCollection.insert(
+              {
+                role: defaultHomeRoles[i],
+                permission: rolePermissions[j],
+                value: true,
+              }
+            );
+          }
+        }
+      }
+    },
+    addUserToRole(userID, role) {
+      const rolePermissionsCollection = adminCollectionObject('rolePermissions');
+      let rolePermissions = rolePermissionsCollection.find({ role, value: true }).fetch();
+
+      rolePermissions = _.map(
+        rolePermissions, (permission) => permission.permission
+      );
+
+      if (rolePermissions && rolePermissions.length > 0) {
+        Roles.addUsersToRoles(userID, rolePermissions, Roles.GLOBAL_GROUP);
+      }
+    },
+    removeUserFromRole(userID, role) {
+      const rolePermissionsCollection = adminCollectionObject('rolePermissions');
+      let rolePermissions = rolePermissionsCollection.find({ role, value: true }).fetch();
+
+      rolePermissions = _.map(
+        rolePermissions, (permission) => permission.permission
+      );
+
+      rolePermissions = _.filter(
+        rolePermissions, (permission) => {
+          let userRoles = HomeHelpers.getUserRoles(userID);
+          userRoles = _.filter(
+            userRoles, (userRole) => {
+              if (userRole === role) {
+                return false;
+              }
+              return true;
+            }
+          );
+
+          if (userRoles && userRoles.length > 0) {
+            const otherRolesWithPermission = rolePermissionsCollection.find(
+              {
+                permission,
+                role: { $in: userRoles },
+                value: true,
+              }
+            ).fetch();
+            if (otherRolesWithPermission.length > 0) {
+              return false;
+            }
+          }
+
+          return true;
+        }
+      );
+
+      if (rolePermissions && rolePermissions.length > 0) {
+        Roles.removeUsersFromRoles(userID, rolePermissions, Roles.GLOBAL_GROUP);
+      }
+    },
+    updateRolePermissions(data) {
+      const dataz = data;
+      const rolePermissions = Meteor.call('getAllRolePermissions');
+      const rolePermissionsCollection = adminCollectionObject('rolePermissions');
+
+      for (let i = 0; i < rolePermissions.length; i++) {
+        let flag = false;
+        let last = false;
+        for (let j = 0; j < dataz; j++) {
+          if (dataz[j].name ===
+              `rolePermissions[${rolePermissions[i].permission}][${rolePermissions[i].role}]`) {
+            flag = j;
+            break;
+          }
+          last = j;
+        }
+
+        if (flag) {
+          rolePermissionsCollection.upsert(
+            { role: rolePermissions[i].role, permission: rolePermissions[i].permission }, {
+              $set: {
+                role: rolePermissions[i].role,
+                permission: rolePermissions[i].permission,
+                value: true,
+              },
+            }
+          );
+          dataz[flag].processed = true;
+        } else {
+          rolePermissionsCollection.upsert(
+            { role: rolePermissions[i].role, permission: rolePermissions[i].permission }, {
+              $set: {
+                role: rolePermissions[i].role,
+                permission: rolePermissions[i].permission,
+                value: false,
+              },
+            }
+          );
+          if (last) {
+            dataz[last].processed = true;
+          }
+        }
+      }
+
+      for (let i = 0; i < dataz.length; i++) {
+        if (! dataz[i].processed) {
+          rolePermissionsCollection.upsert(
+            { role: dataz[i].role, permission: dataz[i].permission }, {
+              $set: {
+                role: dataz[i].role,
+                permission: dataz[i].permission,
+                value: false,
+              },
+            }
+          );
+          dataz[i].processed = false;
+        }
+      }
+
+      return dataz;
+    },
+    resetRolePermissions() {
+      const rolePermissionsCollection = adminCollectionObject('rolePermissions');
+      rolePermissionsCollection.remove({});
+      Meteor.call('generateHomeRolePermissions');
+    },
+    saveAdminSettings(settings) {
+      check(settings, Schemas.adminSettings);
+
+      let value = '';
+      if (typeof settings.hmisAPI !== 'undefined'
+          && typeof settings.hmisAPI.trustedAppID !== 'undefined') {
+        value = settings.hmisAPI.trustedAppID;
+      }
+
+      const optionsCollection = adminCollectionObject('options');
+      optionsCollection.upsert(
+        { option_name: 'trustedAppID' }, {
+          $set: {
+            option_name: 'trustedAppID',
+            option_value: value,
+          },
+        }
+      );
+
+      value = '';
+      if (typeof settings.hmisAPI !== 'undefined'
+          && typeof settings.hmisAPI.trustedAppSecret !== 'undefined') {
+        value = settings.hmisAPI.trustedAppSecret;
+      }
+
+      optionsCollection.upsert(
+        { option_name: 'trustedAppSecret' }, {
+          $set: {
+            option_name: 'trustedAppSecret',
+            option_value: value,
+          },
+        }
+      );
+    },
+    addSurvey(title, active, copy, surveyCopyID, stype, created) {
+      const surveyCollection = adminCollectionObject('surveys');
+      const surveyID = surveyCollection.insert(
+        {
+          title,
+          active,
+          copy,
+          surveyCopyID,
+          stype,
+          created,
+        }
+      );
+      return surveyID;
+    },
+    updateSurvey(surveyID, title, stype, active) {
+      const surveyCollection = adminCollectionObject('surveys');
+      surveyCollection.update(surveyID, { $set: { title, stype, active } });
+    },
+    updateCreatedSurvey(surveyID, created) {
+      const surveyCollection = adminCollectionObject('surveys');
+      surveyCollection.update(surveyID, { $set: { created } });
+    },
+    removeSurvey(surveyID) {
+      const surveyCollection = adminCollectionObject('surveys');
+      surveyCollection.remove({ _id: surveyID });
+    },
+    addQuestion(category, name, question, dataType, options, qtype, audience, locked, isCopy) {
+      const questionCollection = adminCollectionObject('questions');
+      questionCollection.insert(
+        {
+          category,
+          name,
+          question,
+          options,
+          dataType,
+          qtype,
+          audience,
+          locked,
+          isCopy,
+        }
+      );
+    },
+    updateQuestion(
+      questionID,
+      category,
+      name,
+      question,
+      dataType,
+      options,
+      qtype,
+      audience,
+      locked,
+      isCopy
+    ) {
+      const questionCollection = adminCollectionObject('questions');
+      questionCollection.update(
+        questionID, {
+          $set: {
+            category,
+            name,
+            question,
+            options,
+            dataType,
+            qtype,
+            audience,
+            locked,
+            isCopy,
+          },
+        }
+      );
+    },
+    removeQuestion(questionID) {
+      const questionCollection = adminCollectionObject('questions');
+      questionCollection.remove({ _id: questionID });
+    },
+    addSurveyQuestionMaster(
+      surveyTitle,
+      surveyID,
+      sectionID,
+      allowSkip,
+      contentType,
+      content,
+      order
+    ) {
+      const surveyQuestionsMasterCollection = adminCollectionObject('surveyQuestionsMaster');
+      const surveyQues = surveyQuestionsMasterCollection.insert(
+        {
+          surveyTitle,
+          surveyID,
+          sectionID,
+          allowSkip,
+          contentType,
+          content,
+          order,
+        }
+      );
+      return surveyQues;
+    },
+    updateSurveyQuestionMaster(_id, content) {
+      const surveyQuestionsMasterCollection = adminCollectionObject('surveyQuestionsMaster');
+      surveyQuestionsMasterCollection.update({ _id }, { $set: { content } });
+    },
+    updateSurveyQuestionMasterTitle(id, surveyTitle) {
+      const surveyQuestionsMasterCollection = adminCollectionObject('surveyQuestionsMaster');
+      surveyQuestionsMasterCollection.update(
+        { surveyID: id },
+        { $set: { surveyTitle } }, { multi: true }
+      );
+    },
+    removeSurveyQuestionMaster(id) {
+      const surveyQuestionsMasterCollection = adminCollectionObject('surveyQuestionsMaster');
+      surveyQuestionsMasterCollection.remove({ _id: id });
+    },
+    addClient(
+      firstName,
+      middleName,
+      lastName,
+      suffix,
+      ssn,
+      dob,
+      race,
+      ethnicity,
+      gender,
+      veteranStatus,
+      disablingConditions,
+      residencePrior,
+      entryDate,
+      exitDate,
+      destination,
+      householdId,
+      relationship,
+      location,
+      shelter
+    ) {
+      const clientInfoCollection = adminCollectionObject('clientInfo');
+      const clientRecords = clientInfoCollection.insert(
+        {
+          firstName,
+          middleName,
+          lastName,
+          suffix,
+          ssn,
+          dob,
+          race,
+          ethnicity,
+          gender,
+          veteranStatus,
+          disablingConditions,
+          residencePrior,
+          entryDate,
+          exitDate,
+          destination,
+          householdId,
+          relationship,
+          location,
+          shelter,
+        }
+      );
+      return clientRecords;
+    },
+    updateClient(
+      clientInfoID,
+      firstName,
+      middleName,
+      lastName,
+      suffix,
+      ssn,
+      dob,
+      race,
+      ethnicity,
+      gender,
+      veteranStatus,
+      disablingConditions,
+      residencePrior,
+      entryDate,
+      exitDate,
+      destination,
+      householdId,
+      relationship,
+      location,
+      shelter
+    ) {
+      const clientInfoCollection = adminCollectionObject('clientInfo');
+      clientInfoCollection.update(
+        clientInfoID, {
+          $set: {
+            firstName,
+            middleName,
+            lastName,
+            suffix,
+            ssn,
+            dob,
+            race,
+            ethnicity,
+            gender,
+            veteranStatus,
+            disablingConditions,
+            residencePrior,
+            entryDate,
+            exitDate,
+            destination,
+            householdId,
+            relationship,
+            location,
+            shelter,
+          },
+        }
+      );
+    },
+    removeClient(clientInfoID) {
+      const questionCollection = adminCollectionObject('clientInfo');
+      questionCollection.remove({ _id: clientInfoID });
+    },
+    removeSurveyCopyQuestionMaster(surveyCopyTitle) {
+      const surveyQuestionsMasterCollection = adminCollectionObject('surveyQuestionsMaster');
+      surveyQuestionsMasterCollection.remove({ surveyTitle: surveyCopyTitle });
+    },
+    addSurveyResponse(surveyID, clientID, audience, userID, mainSectionObject, status) {
+      const responsesCollection = adminCollectionObject('responses');
+      const responseRecords = responsesCollection.insert(
+        {
+          clientID,
+          audience,
+          surveyID,
+          userID,
+          responsestatus: status,
+          section: mainSectionObject,
+        }
+      );
+      return responseRecords;
+    },
+    updateSurveyResponse(responseID, surveyID, clientID, userID, mainSectionObject, status) {
+      const responsesCollection = adminCollectionObject('responses');
+      const responseRecords = responsesCollection.update(
+        {
+          _id: responseID,
+        }, {
+          $set: {
+            clientID,
+            surveyID,
+            userID,
+            responsestatus: status,
+            section: mainSectionObject,
+          },
+        }
+      );
+      return responseRecords;
+    },
+    addClientToHMIS(clientID) {
+      const clientInfoCollection = adminCollectionObject('clientInfo');
+      const client = clientInfoCollection.findOne({ _id: clientID });
+
+      const personalId = HMISAPI.createClient(client);
+
+      let flag = false;
+
+      if (personalId) {
+        clientInfoCollection.update(
+          {
+            _id: client._id,
+          }, {
+            $set: {
+              personalId,
+            },
+          }
+        );
+        flag = true;
+      }
+      return flag;
+    },
+  }
+);
