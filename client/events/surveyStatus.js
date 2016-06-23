19503358c4b0fb54042401f15e2a49e2bb259752
anush24 <agovindan89@gmail.com>
2016-06-23 13:48:35 -0700

    fixes EsLinst errors for events/surveyStatus.js

--- client/events/surveyStatus.js
+++ client/events/surveyStatus.js
@@ -2,24 +2,15 @@
  * Created by Kavi on 5/19/16.
  */
 Template.surveyStatusRow.events({
-
-    'click .survey_route': function (evt,tmpl) {
-
-        var response_id = tmpl.data._id;
-        var responseCollection = adminCollectionObject("responses");
-        var response_records = responseCollection.find({_id:response_id}).fetch();
-
-        for(var i in response_records){
-
-            var id = response_records[i]._id;
-			var audience=response_records[i].audience
-            Router.go('LogSurveyView', {_id: id},{query: {
-				'audience': audience
-				}
-			});
-
-        }
-
+  'click .survey_route'(evt, tmpl) {
+    const responseID = tmpl.data._id;
+    const responseCollection = adminCollectionObject('responses');
+    const responseRecords = responseCollection.find({ _id: responseID }).fetch();
+    for (let i = 0; i < responseRecords.length; i++) {
+      {
+        const id = responseRecords[i]._id;
+        const audience = responseRecords[i].audience;
+        Router.go('LogSurveyView', { _id: id }, { query: { audience } });
+      }
     }
-
-});
+  } });
