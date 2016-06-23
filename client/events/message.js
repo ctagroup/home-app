4c203777cf384dcf699e896156e863fe07e3219e
Udit Desai <desaiuditd@gmail.com>
2016-06-23 16:32:50 -0700

    fixes ESLint issues for client folder

--- client/events/message.js
+++ client/events/message.js
@@ -1,12 +1,14 @@
-Template.newMessage.events({
-  'submit .new-message'(e) {
-    e.preventDefault();
-    const contentText = $(e.target).find('[name=content]');
-    const msg = {
-      name: 'Meteor Chatter',
-      content: contentText.val(),
-    };
-    msg._id = Messages.insert(msg);
-    contentText.val('');
-  },
-});
+Template.newMessage.events(
+  {
+    'submit .new-message'(e) {
+      e.preventDefault();
+      const contentText = $(e.target).find('[name=content]');
+      const msg = {
+        name: 'Meteor Chatter',
+        content: contentText.val(),
+      };
+      msg._id = Messages.insert(msg);
+      contentText.val('');
+    },
+  }
+);
--- client/events/searchClient.js
+++ client/events/searchClient.js
@@ -2,9 +2,11 @@
  * Created by udit on 20/06/16.
  */
 
-Template.searchClient.onRendered(() => {
-  Meteor.typeahead.inject();
-});
+Template.searchClient.onRendered(
+  () => {
+    Meteor.typeahead.inject();
+  }
+);
 
 Template.searchClient.events(
   {
