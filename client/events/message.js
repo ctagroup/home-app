875a2782e0de7066e775ff9dd6b6f901a8ef247e
anush24 <agovindan89@gmail.com>
2016-06-23 14:27:34 -0700

    fixes EsLint errors for events/message.js

--- client/events/message.js
+++ client/events/message.js
@@ -1,15 +1,12 @@
 Template.newMessage.events({
-    'submit .new-message': function(e) {
-        e.preventDefault();
-
-        var contentText = $(e.target).find('[name=content]');
-
-        var msg = {
-            name:    "Meteor Chatter",
-            content: contentText.val()
-        };
-
-        msg._id = Messages.insert(msg);
-        contentText.val('');
-    }
+  'submit .new-message'(e) {
+    e.preventDefault();
+    const contentText = $(e.target).find('[name=content]');
+    const msg = {
+      name: 'Meteor Chatter',
+      content: contentText.val(),
+    };
+    msg._id = Messages.insert(msg);
+    contentText.val('');
+  },
 });
