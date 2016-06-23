0ec3bbd97c5a55437cf473842e80130de8877f39
Udit Desai <desaiuditd@gmail.com>
2016-06-22 18:27:25 -0700

    fixes ESLint issues for searchClient.js

--- client/helpers/searchClient.js
+++ client/helpers/searchClient.js
@@ -3,43 +3,51 @@
  */
 
 Template.searchClient.helpers(
-	{
-		searchClient: function ( query, sync, callback ) {
-			Meteor.call('searchClient', query, {}, function(err, res) {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				callback( res.map( function( v ) {
-					var fn = (v && v.firstName) ? v.firstName.trim() : '';
-					var mn = (v && v.middleName) ? v.middleName.trim() : '';
-					var ln = (v && v.lastName) ? v.lastName.trim() : '';
-					v.value = fn + " " + mn + " " + ln;
-					v.value = v.value.trim();
-					return v;
-				} ) );
-			});
-		},
-		clientSelected: function (event, dataObject) {
-			if ( dataObject.clientNotFound ) {
-				$('#search-client-keyword').val(dataObject.query).change();
-				Router.go('createClient', {}, { query : 'firstName='+dataObject.query } );
-			} else {
-				var query = {};
-				if ( dataObject.isHMISClient ) {
-					query.query = 'isHMISClient=true';
-				}
-				Router.go('viewClient', { _id: dataObject._id }, query );
-			}
-		},
-		getRecentClients: function () {
-			return Session.get("recentClients") || [];
-		},
-		alertMessages: function () {
-			var params = Router.current().params;
-			if ( params && params.query && params.query.deleted ) {
-				return "<p class='notice bg-success text-success'>Client is removed successfully.</p>";
-			}
-		}
-	}
+  {
+    searchClient(query, sync, callback) {
+      Meteor.call(
+        'searchClient', query, {}, (err, res) => {
+          if (err) {
+            logger.log(err);
+            return;
+          }
+          callback(
+            res.map(
+              (v) => {
+                const vz = v;
+                const fn = (vz && vz.firstName) ? vz.firstName.trim() : '';
+                const mn = (vz && vz.middleName) ? vz.middleName.trim() : '';
+                const ln = (vz && vz.lastName) ? vz.lastName.trim() : '';
+                vz.value = `${fn} ${mn} ${ln}`;
+                vz.value = vz.value.trim();
+                return vz;
+              }
+            )
+          );
+        }
+      );
+    },
+    clientSelected(event, dataObject) {
+      if (dataObject.clientNotFound) {
+        $('#search-client-keyword').val(dataObject.query).change();
+        Router.go('createClient', {}, { query: `firstName=${dataObject.query}` });
+      } else {
+        const query = {};
+        if (dataObject.isHMISClient) {
+          query.query = 'isHMISClient=true';
+        }
+        Router.go('viewClient', { _id: dataObject._id }, query);
+      }
+    },
+    getRecentClients() {
+      return Session.get('recentClients') || [];
+    },
+    alertMessages() {
+      const params = Router.current().params;
+      if (params && params.query && params.query.deleted) {
+        return '<p class="notice bg-success text-success">Client is removed successfully.</p>';
+      }
+      return '';
+    },
+  }
 );
