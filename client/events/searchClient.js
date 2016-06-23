6ec48ce7ffdfe32c5d598f372006aa574c0ec44d
anush24 <agovindan89@gmail.com>
2016-06-23 11:17:41 -0700

    fixes ESLint issues for events/searchClient.js

--- client/events/searchClient.js
+++ client/events/searchClient.js
@@ -2,29 +2,28 @@
  * Created by udit on 20/06/16.
  */
 
-Template.searchClient.onRendered( function() {
-	Meteor.typeahead.inject();
-} );
+Template.searchClient.onRendered(() => {
+  Meteor.typeahead.inject();
+});
 
 Template.searchClient.events(
-	{
-		'click .search-btn': function(event, template){
-			event.preventDefault();
-			var fullname = template.find('input.typeahead.tt-input').value;
-			console.log(fullname);
-			if(fullname != null) {
-				console.log(fullname);
-				var fnln = fullname.split(" ");
-				var clientRecord = clientInfo.findOne({firstName : fnln[0], lastName : fnln[1]});
-				console.log(clientRecord);
-				if(clientRecord){
-					var clientID = clientRecord._id;
-					console.log(clientID);
-					Router.go('viewClient', {_id: clientID});
-				} else{
-					console.log("The entered name is not in the local MongoDB");
-				}
-			}
-		}
-	}
+  {
+    'click .search-btn'(event, template) {
+      event.preventDefault();
+      const fullname = template.find('input.typeahead.tt-input').value;
+      if (fullname != null) {
+        // console.log(fullname);
+        const fnln = fullname.split('');
+        const clientRecord = clientInfo.findOne({ firstName: fnln[0], lastName: fnln[1] });
+        // console.log(clientRecord);
+        if (clientRecord) {
+          const clientID = clientRecord._id;
+          // console.log(clientID);
+          Router.go('viewClient', { _id: clientID });
+        } else {
+          // console.log('The entered name is not in the local MongoDB');
+        }
+      }
+    },
+  }
 );
