0cf871d7c82e3008e21f6e3bc9f1754858519c89
Udit Desai <desaiuditd@gmail.com>
2016-07-07 11:50:36 -0700

    updates logo in mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -15,5 +15,19 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-  iphone: '/public/imgs/logo.png',
+  iphone_2x: 'public/imgs/logo.png', // (120x120)
+  iphone_3x: 'public/imgs/logo.png', // (180x180)
+  ipad: 'public/imgs/logo.png', // (76x76)
+  ipad_2x: 'public/imgs/logo.png', // (152x152)
+  ipad_pro: 'public/imgs/logo.png', // (167x167)
+  ios_settings: 'public/imgs/logo.png', // (29x29)
+  ios_settings_2x: 'public/imgs/logo.png', // (58x58)
+  ios_settings_3x: 'public/imgs/logo.png', // (87x87)
+  ios_spotlight: 'public/imgs/logo.png', // (40x40)
+  ios_spotlight_2x: 'public/imgs/logo.png', // (80x80)
+  android_mdpi: 'public/imgs/logo.png', // (48x48)
+  android_hdpi: 'public/imgs/logo.png', // (72x72)
+  android_xhdpi: 'public/imgs/logo.png', // (96x96)
+  android_xxhdpi: 'public/imgs/logo.png', // (144x144)
+  android_xxxhdpi: 'public/imgs/logo.png', // (192x192)
 });
