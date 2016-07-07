b934dc5a63caf952e22fffd06c5a2184231ced68
Udit Desai <desaiuditd@gmail.com>
2016-07-07 11:20:20 -0700

    adds mobile packages and mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by udit on 07/07/16.
+ */
+
+// This section sets up some basic app metadata,
+// the entire section is optional.
+App.info({
+  id: 'org.ctagroup.home',
+  name: 'HOME',
+  description: 'Plays Well With Others',
+  author: 'Community Technology Alliace',
+  email: 'javier@ctagroup.org',
+  website: 'http://home.ctagroup.org',
+});
+
+// Set up resources such as icons and launch screens.
+App.icons({
+  iphone: '/public/imgs/logo.png',
+});
