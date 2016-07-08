c4bfdc0012ae16b1666c05b1fff4a018d29d4f1a
Udit Desai <desaiuditd@gmail.com>
2016-07-08 12:34:48 -0700

    fixes external assets access in mobile

--- mobile-config.js
+++ mobile-config.js
@@ -32,7 +32,11 @@ App.icons({
   android_xxxhdpi: 'public/imgs/logo.png', // (192x192)
 });
 
-App.accessRule('https://*gravatar.com/*', { type: 'network' });
-App.accessRule('https://*loremflickr.com/*', { type: 'network' });
-App.accessRule('http://*gravatar.com/*', { type: 'network' });
-App.accessRule('http://*loremflickr.com/*', { type: 'network' });
+App.accessRule('https://gravatar.com/*', { type: 'network' });
+App.accessRule('https://www.gravatar.com/*', { type: 'network' });
+App.accessRule('https://loremflickr.com/*', { type: 'network' });
+App.accessRule('https://www.loremflickr.com/*', { type: 'network' });
+App.accessRule('http://gravatar.com/*', { type: 'network' });
+App.accessRule('http://www.gravatar.com/*', { type: 'network' });
+App.accessRule('http://loremflickr.com/*', { type: 'network' });
+App.accessRule('http://www.loremflickr.com/*', { type: 'network' });
