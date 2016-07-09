61b15c24ac56be921cec6b9f163f54b45b821153
Udit Desai <desaiuditd@gmail.com>
2016-07-08 18:55:09 -0700

    refactors LocationTracker & fixes Google Maps for Location Tracker

--- client/events/admin/userEdit.js
+++ client/events/admin/userEdit.js
@@ -11,11 +11,39 @@ Template.AdminDashboardusersEdit.onRendered(() => {
 
   // We can use the `ready` callback to interact with the map API once the map is ready.
   GoogleMaps.ready('locationHistory', (map) => {
-    // Add a marker to the map once it's ready
-    const marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance,
+    const poly = new google.maps.Polyline({
+      strokeColor: '#428bca',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
     });
-    logger.log(marker);
+
+    poly.setMap(map.instance);
+
+    const bounds  = new google.maps.LatLngBounds();
+
+    const userID = Session.get('admin_id');
+    const locationHistory = LocationTracker.getLocationHistory(userID);
+
+    for (let i = 0; i < locationHistory.length; i++) {
+      const path = poly.getPath();
+
+      const latLong = new google.maps.LatLng(locationHistory[i].position.lat, locationHistory[i].position.long);
+
+      // Because path is an MVCArray, we can simply append a new coordinate
+      // and it will automatically appear.
+      path.push(latLong);
+
+      // Add a new marker at the new plotted point on the polyline.
+      var marker = new google.maps.Marker({
+        position: latLong,
+        title: locationHistory.timestamp,
+        map: map.instance,
+      });
+
+      bounds.extend(latLong);
+    }
+
+    map.instance.fitBounds(bounds);       // auto-zoom
+    map.instance.panToBounds(bounds);     // auto-center
   });
 });
