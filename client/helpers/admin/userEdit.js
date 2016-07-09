ff59c432bc2ace26aaf5b71c20a901878a420da6
Udit Desai <desaiuditd@gmail.com>
2016-07-08 18:55:09 -0700

    refactors LocationTracker & fixes Google Maps for Location Tracker

--- client/helpers/admin/userEdit.js
+++ client/helpers/admin/userEdit.js
@@ -7,10 +7,20 @@ Template.AdminDashboardusersEdit.helpers(
     locationHistoryMapOptions() {
       // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
+
+        let center = new google.maps.LatLng(0, -180);
+
+        const userID = Session.get('admin_id');
+
+        if (userID) {
+          const lastPosition = LocationTracker.getLastPosition(userID);
+          center = new google.maps.LatLng(lastPosition.lat, lastPosition.long);
+        }
+
         // Map initialization options
         return {
-          center: new google.maps.LatLng(0, -180),
-          zoom: 8,
+          center,
+          zoom: 5,
         };
       }
 
