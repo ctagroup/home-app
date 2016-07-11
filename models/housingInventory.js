87aacf740bab557891c3f085a6d05e7884a91d27
Udit Desai <desaiuditd@gmail.com>
2016-07-11 13:50:01 -0700

    adds Housing Inventory module

---
+++ models/housingInventory.js
@@ -0,0 +1,9 @@
+housingInventory = new Meteor.Collection('housingInventory');
+
+Schemas.housingInventory = new SimpleSchema(
+  {
+
+  }
+);
+
+housingInventory.attachSchema(Schemas.housingInventory);
