be2297ef8328bc7e7d6f550f7c8143aeb917d60e
Udit Desai <desaiuditd@gmail.com>
2016-07-12 14:42:14 -0700

    stops user location tracker

--- client/helpers/_common.js
+++ client/helpers/_common.js
@@ -2,35 +2,35 @@
  * Created by udit on 08/07/16.
  */
 
-Tracker.autorun(() => {
-  if (Meteor.user()) {
-    const position = Geolocation.currentLocation();
+// Tracker.autorun(() => {
+  // if (Meteor.user()) {
+    // const position = Geolocation.currentLocation();
 
-    if (position) {
-      const coords = {
-        accuracy: position.coords.accuracy,
-        lat: position.coords.latitude,
-        long: position.coords.longitude,
-      };
-      Meteor.call(
-        'addUserLocation',
-        Meteor.user()._id,
-        new Date(position.timestamp),
-        coords,
-        (error, result) => {
-          if (error) {
-            logger.log(error);
-          } else {
-            logger.log(result);
-          }
-        }
-      );
-    } else {
-      logger.log('position not found.');
-      logger.log(Geolocation.error());
-    }
-  }
-});
+    // if (position) {
+      // const coords = {
+      //   accuracy: position.coords.accuracy,
+      //   lat: position.coords.latitude,
+      //   long: position.coords.longitude,
+      // };
+      // Meteor.call(
+      //   'addUserLocation',
+      //   Meteor.user()._id,
+      //   new Date(position.timestamp),
+      //   coords,
+      //   (error, result) => {
+      //     if (error) {
+      //       logger.log(error);
+      //     } else {
+      //       logger.log(result);
+      //     }
+      //   }
+      // );
+    // } else {
+      // logger.log('position not found.');
+      // logger.log(Geolocation.error());
+    // }
+  // }
+// });
 
 UI.registerHelper(
   'currentUserCan', (cap) => Roles.userIsInRole(Meteor.user(), cap)
