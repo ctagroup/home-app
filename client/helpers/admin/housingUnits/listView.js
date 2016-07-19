dd3ed0ba7d85c95199e236e5040e3729f10c5af7
Udit Desai <desaiuditd@gmail.com>
2016-07-19 15:06:50 -0700

    adds table for housing units list view

--- client/helpers/admin/housingUnits/listView.js
+++ client/helpers/admin/housingUnits/listView.js
@@ -7,5 +7,16 @@ Template.housingUnitsListView.helpers(
     hasHousingUnits() {
       return housingUnits.find({}).fetch();
     },
+    housingUnitsTableOptions() {
+      return {
+        columns: AdminConfig.collections.housingUnits.tableColumns,
+        dom: AdminConfig.adminTablesDom
+      };
+    },
+    housingUnitsData() {
+      return () => {
+        return housingUnits.find({}).fetch();
+      }
+    },
   }
 );
