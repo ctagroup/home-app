b59ae9b964e7d7808470a54fa5dfaa69940947a1
anush24 <agovindan89@gmail.com>
2016-07-20 12:27:18 -0700

    Added Housing Match GUI framework without HMIS

---
+++ client/helpers/admin/housingMatch/housingMatchListView.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Anush-PC on 7/19/2016.
+ */
+
+Template.housingMatchListView.helpers(
+  {
+    hasHousingMatch() {
+      return housingMatch.find({}).fetch();
+    },
+    housingMatchTableOptions() {
+      return {
+        columns: AdminConfig.collections.housingMatch.tableColumns,
+        dom: AdminConfig.adminTablesDom,
+      };
+    },
+    housingMatchData() {
+      return () => housingMatch.find({}).fetch();
+    },
+  }
+);
