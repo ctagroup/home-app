1e0eecf580e7691b3c2286ccb1e57138a3f9b264
anush24 <agovindan89@gmail.com>
2016-07-29 11:09:13 -0700

    Global Household - basic read and view from HMIS

--- server/publish/admin/globalHousehold.js
+++ server/publish/admin/globalHousehold.js
@@ -7,34 +7,57 @@ Meteor.publish(
     const self = this;
 
     let globalHousehold = [];
+    if (self.userId) {
+      HMISAPI.setCurrentUserId(self.userId);
 
-    // if (this.userId) {
-    // } else {
-    //   HMISAPI.setCurrentUserId('');
-    // }
-
-    globalHousehold = [
-      {
-        globalHouseHoldId: null,
-        headOfHouseholdId: 'asdf-asdf-assdfasfd-dd',
-        dateCreated: '6/06/2016',
-        dateUpdated: '6/06/2016',
-        userCreate: 'asdf-adf-asdsf',
-        userUpdate: 'asdf-adfasf-asdfasds',
-      },
-      {
-        globalHouseHoldId: null,
-        headOfHouseholdId: 'asdf-asdf-assdfasfd-dd-sadf',
-        dateCreated: '6/06/2016',
-        dateUpdated: '6/06/2016',
-        userCreate: 'asdf-adf-asdsf',
-        userUpdate: 'asdf-adfasf-asdfasds',
-      },
-    ];
+      globalHousehold = HMISAPI.getGlobalHouseholdForPublish();
+    } else {
+      HMISAPI.setCurrentUserId('');
+    }
+    // globalHousehold = [
+    //   {
+    //     globalHouseHoldId: null,
+    //     headOfHouseholdId: 'asdf-asdf-assdfasfd-dd',
+    //     dateCreated: '6/06/2016',
+    //     dateUpdated: '6/06/2016',
+    //     userCreate: 'asdf-adf-asdsf',
+    //     userUpdate: 'asdf-adfasf-asdfasds',
+    //   },
+    //   {
+    //     globalHouseHoldId: null,
+    //     headOfHouseholdId: 'asdf-asdf-assdfasfd-dd-sadf',
+    //     dateCreated: '6/06/2016',
+    //     dateUpdated: '6/06/2016',
+    //     userCreate: 'asdf-adf-asdsf',
+    //     userUpdate: 'asdf-adfasf-asdfasds',
+    //   },
+    // ];
 
     _.each(globalHousehold, (item) => {
-      self.added('globalHousehold', Random.id(), item);
+      self.added('globalHousehold', item.globalHouseholdId, item);
     });
+
+    self.ready();
+  }
+);
+
+Meteor.publish(
+  'singleGlobalHousehold', function publishSingleGlobalHousehold(globalHouseholdId) {
+    const self = this;
+
+    let singleGlobalHousehold = false;
+
+    if (self.userId) {
+      HMISAPI.setCurrentUserId(self.userId);
+
+      singleGlobalHousehold = HMISAPI.getSingleGlobalHouseholdForPublish(globalHouseholdId);
+    } else {
+      HMISAPI.setCurrentUserId('');
+    }
+
+    if (singleGlobalHousehold) {
+      self.added('globalHousehold', singleGlobalHousehold.globalHousehold, singleGlobalHousehold);
+    }
 
     self.ready();
   }
