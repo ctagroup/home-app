e9f083fdc0fdb819413a4c36f7c01ff4e8f1b297
anush24 <agovindan89@gmail.com>
2016-08-08 14:13:27 -0700

    Fixed eslint issues

--- client/events/appDeleteModal.js
+++ client/events/appDeleteModal.js
@@ -6,15 +6,13 @@ Template.AppDeleteModal.events({
   'click #confirm-delete'() {
     const collection = Session.get('admin_collection_name');
     const _id = Session.get('admin_id');
-    console.log(collection);
     if (collection === 'globalHousehold') {
       Meteor.call('deleteHousehold', _id, (e, r) => {
         logger.log(e);
         logger.log(r);
       });
       $('#app-delete-modal').modal('hide');
-      // location.reload();
-      Meteor._reload.reload();
+      location.reload();
     } else {
       Meteor.call('adminRemoveDoc', collection, _id, (e, r) => {
         logger.log(e);
@@ -22,6 +20,5 @@ Template.AppDeleteModal.events({
       });
       $('#app-delete-modal').modal('hide');
     }
-
   },
 });
