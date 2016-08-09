5ea6d6d1773558db271cb9b2961adca49aed7859
Udit Desai <desaiuditd@gmail.com>
2016-08-08 17:39:41 -0700

    fixes hmis client back button

--- client/events/response/createResponse.js
+++ client/events/response/createResponse.js
@@ -2,6 +2,8 @@
  * Created by udit on 02/08/16.
  */
 
+const querystring = require('querystring');
+
 Template.createResponse.events(
   {
     'click .pause_survey': (evt, tmpl) => {
@@ -11,7 +13,20 @@ Template.createResponse.events(
       ResponseHelpers.saveResponse('Submit', tmpl);
     },
     'click .cancel_survey': (evt, tmpl) => {
-      Router.go('selectSurvey', { _id: tmpl.data.client._id });
+      const query = {};
+
+      if (Router.current().params && Router.current().params.query
+          && Router.current().params.query.isHMISClient && Router.current().params.query.link) {
+        const url = encodeURIComponent(Router.current().params.query.link);
+        query.isHMISClient = true;
+        query.link = url;
+      }
+
+      Router.go(
+        'selectSurvey',
+        { _id: tmpl.data.client._id },
+        { query: querystring.stringify(query) }
+      );
     },
   }
 );
