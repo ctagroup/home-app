755d7b8811a5dabea9eacdc8902aaa7e5a35aed0
manoj2509 <manojparihar09@gmail.com>
2016-08-26 20:07:22 -0700

    Updates Housing unit list view as discussed. Displays project Names for Project Id's.

--- server/publish/housingUnits.js
+++ server/publish/housingUnits.js
@@ -10,19 +10,29 @@ Meteor.publish(
 
     if (self.userId) {
       HMISAPI.setCurrentUserId(self.userId);
-
       const response = HMISAPI.getHousingUnitsForPublish();
       housingUnits = response.content;
+      // according to the content received.
+      logger.info(housingUnits.length);
+      for (let i = 1; i < response.page.totalPages; i++) {
+        const temp = HMISAPI.getHousingUnitsForPublish(i);
+        housingUnits.push.apply(housingUnits, temp.content);
+        logger.info(`Temp: ${housingUnits.length}`);
+        _.each(temp, (item) => {
+          const itemz = item;
+          itemz.project = HMISAPI.getProject(item.projectId);
+          self.added('housingUnits', itemz.housingInventoryId, itemz);
+        });
+        self.ready();
+      }
     } else {
       HMISAPI.setCurrentUserId('');
     }
-
     _.each(housingUnits, (item) => {
       const itemz = item;
       itemz.project = HMISAPI.getProject(item.projectId);
       self.added('housingUnits', itemz.housingInventoryId, itemz);
     });
-
     self.ready();
   }
 );
