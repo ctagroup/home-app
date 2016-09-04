f9904f87dc17650c7d284728d3a1ce39ecdb60ff
Udit Desai <desaiuditd@gmail.com>
2016-09-04 16:51:26 -0700

    adds projectGroups from HMIS in user form

---
+++ server/publish/projectGroups.js
@@ -0,0 +1,31 @@
+/**
+ * Created by udit on 02/09/16.
+ */
+
+Meteor.publish(
+  'projectGroups', function publishUsersNew() {
+    const self = this;
+
+    let projectGroups = [];
+
+    if (self.userId) {
+      HMISAPI.setCurrentUserId(self.userId);
+      const response = HMISAPI.getProjectGroups(0, 30);
+      projectGroups = response.projectGroups;
+      // according to the content received.
+      logger.info(projectGroups.length);
+      for (let i = 0; i*30 < response.pagination.total; i++) {
+        const temp = HMISAPI.getProjectGroups(i*30, 30);
+        projectGroups.push.apply(projectGroups, temp.projectGroups);
+        logger.info(`Temp: ${projectGroups.length}`);
+        _.each(temp.projectGroups, (item) => {
+          const tempItem = item;
+          self.added('projectGroups', tempItem.projectGroupId, tempItem);
+        });
+      }
+      self.ready();
+    } else {
+      HMISAPI.setCurrentUserId('');
+    }
+  }
+);
