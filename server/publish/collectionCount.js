1f85ccd4469b0d40be15fb72b0eccb360f5410c5
Udit Desai <desaiuditd@gmail.com>
2016-10-07 10:27:54 -0700

    fixes dashboard count for housing units and global households

--- server/publish/collectionCount.js
+++ server/publish/collectionCount.js
@@ -8,27 +8,32 @@ Meteor.publish(
     const self = this;
     _.each(
       AdminTables, (table, name) => {
-        let count;
-        let ready;
-        const id = new Mongo.ObjectID;
-        count = 0;
-        ready = false;
-        handles.push(
-          table.collection.find().observeChanges(
-            {
-              added() {
-                count += 1;
-                return ready && self.changed('collectionsCount', id, { count });
-              },
-              removed() {
-                count -= 1;
-                return ready && self.changed('collectionsCount', id, { count });
-              },
-            }
-          )
-        );
-        ready = true;
-        return self.added('collectionsCount', id, { collection: name, count });
+
+        if (HomeConfig.collections[name].fetchCountFromHmis) {
+          HomeConfig.collections[name].fetchCountFromHmis(self, name);
+        } else {
+          let count;
+          let ready;
+          const id = new Mongo.ObjectID;
+          count = 0;
+          ready = false;
+          handles.push(
+            table.collection.find().observeChanges(
+              {
+                added() {
+                  count += 1;
+                  return ready && self.changed('collectionsCount', id, { count });
+                },
+                removed() {
+                  count -= 1;
+                  return ready && self.changed('collectionsCount', id, { count });
+                },
+              }
+            )
+          );
+          ready = true;
+          return self.added('collectionsCount', id, { collection: name, count });
+        }
       }
     );
     self.onStop(
