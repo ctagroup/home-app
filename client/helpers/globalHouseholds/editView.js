b6074b650a99bdb7234d4cb0844c14c320790e5b
Udit Desai <desaiuditd@gmail.com>
2016-10-06 23:41:56 -0700

    fixes eslint

--- client/helpers/globalHouseholds/editView.js
+++ client/helpers/globalHouseholds/editView.js
@@ -6,7 +6,7 @@ Template.globalHouseholdEditView.helpers(
   {
     isActive(currentVal) {
       const globalHousehold = globalHouseholds.findOne({ _id: Router.current().params._id });
-      if (globalHousehold && globalHousehold.inactive == currentVal) {
+      if (globalHousehold && globalHousehold.inactive === currentVal) {
         return 'checked';
       }
       return '';
