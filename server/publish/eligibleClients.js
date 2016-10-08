10594f47b9287e0fb80a43e25e63b6b597e32aa9
Manoj <mparihar@scu.edu>
2016-10-08 00:36:49 -0700

    updates count for eligible Count and housing matching service.

--- server/publish/eligibleClients.js
+++ server/publish/eligibleClients.js
@@ -10,12 +10,6 @@ Meteor.publish(
       HMISAPI.setCurrentUserId(self.userId);
       eligibleClients = HMISAPI.getEligibleClientsForPublish();
       // according to the content received.
-      // Ideally all should be returned here. Keeping it commented for now.
-      // for (let i = 1; i < response.page.totalPages; i += 1) {
-      //   const temp = HMISAPI.getEligibleClientsForPublish(i);
-      //   eligibleClients.push(...temp.content);
-      //   logger.info(`Temp: ${eligibleClients.length}`);
-      // }
       if (eligibleClients) {
         logger.info(`Publishing Eligible Clients: ${JSON.stringify(eligibleClients)}`);
         for (let i = 0; i < eligibleClients.length; i += 1) {
