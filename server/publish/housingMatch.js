3763d1b9df2d4d02578762e5d8e2db4eb56082e4
Manoj <mparihar@scu.edu>
2016-10-08 00:36:49 -0700

    updates count for eligible Count and housing matching service.

--- server/publish/housingMatch.js
+++ server/publish/housingMatch.js
@@ -11,7 +11,7 @@ Meteor.publish(
       HMISAPI.setCurrentUserId(self.userId);
       housingMatch = HMISAPI.getHousingMatchForPublish();
       // according to the content received.
-      // Adding Dummy Data for now.
+      // Adding Dummy Data for testing.
       if (!housingMatch) {
         housingMatch = [
           {
@@ -43,39 +43,9 @@ Meteor.publish(
           self.added('housingMatch', housingMatch[i].reservationId, housingMatch[i]);
         }
       }
-      // Ideally all should be returned here. Keeping it commented for now.
-      // for (let i = 1; i < response.page.totalPages; i += 1) {
-      //   const temp = HMISAPI.getEligibleClientsForPublish(i);
-      //   eligibleClients.push(...temp.content);
-      //   logger.info(`Temp: ${eligibleClients.length}`);
-      // }
       self.ready();
     } else {
       HMISAPI.setCurrentUserId('');
     }
-    // housingMatch = [
-    //   {
-    //     reservation_id: '12345',
-    //     client_id: '456788',
-    //     housing_unit_id: '09876',
-    //     match_date: '2016-10-10',
-    //     user_id: 'admin',
-    //     matchedStatus: true,
-    //   },
-    //   {
-    //     reservation_id: '123456',
-    //     client_id: '4567886',
-    //     housing_unit_id: '09866',
-    //     match_date: '2016-10-11',
-    //     user_id: 'test_user',
-    //     matchedStatus: true,
-    //   },
-    // ];
-    //
-    // _.each(housingMatch, (item) => {
-    //   self.added('housingMatch', Random.id(), item);
-    // });
-    //
-    // self.ready();
   }
 );
