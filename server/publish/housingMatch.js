0b82c00162958f8a200eca1222322874bbaef327
Udit Desai <desaiuditd@gmail.com>
2016-10-08 15:01:30 -0700

    removes dummy data from housing match

--- server/publish/housingMatch.js
+++ server/publish/housingMatch.js
@@ -12,40 +12,17 @@ Meteor.publish(
       housingMatch = HMISAPI.getHousingMatchForPublish();
       // according to the content received.
       // Adding Dummy Data for testing.
-      if (!housingMatch) {
-        housingMatch = [
-          {
-            reservationId: 'f396b640-faf6-4f2f-98db-26a0c84c8e4f',
-            eligibleClients: {
-              clientId: '963cc91f-cd15-4307-ae92-a7a3e6fdba25',
-              surveyScore: 10,
-              matched: false,
-              surveyDate: '09-29-2016 11:05',
-            },
-            matchDate: '2016-10-07',
-            manualMatch: false,
-            inactive: false,
-            dateCreated: '2016-10-07',
-            dateUpdated: '2016-10-07',
-            links: [
-              {
-                rel: 'history',
-                href: '/matches/client/963cc91f-cd15-4307-ae92-a7a3e6fdba25/status',
-              },
-            ],
-          },
-        ];
-      }
-      if (housingMatch) {
-        logger.info(`Publishing Housing Match: ${housingMatch.length}`);
-        for (let i = 0; i < housingMatch.length; i += 1) {
-          // TODO Add client details (Name & link to profile) here.
-          self.added('housingMatch', housingMatch[i].reservationId, housingMatch[i]);
-        }
-      }
-      self.ready();
+      console.log(housingMatch);
     } else {
       HMISAPI.setCurrentUserId('');
     }
+    if (housingMatch) {
+      logger.info(`Publishing Housing Match: ${housingMatch.length}`);
+      for (let i = 0; i < housingMatch.length; i += 1) {
+        // TODO Add client details (Name & link to profile) here.
+        self.added('housingMatch', housingMatch[i].reservationId, housingMatch[i]);
+      }
+    }
+    self.ready();
   }
 );
