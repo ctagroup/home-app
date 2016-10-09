8b9716c2258ce215d8e6d37606011e8dfb71afb8
Udit Desai <desaiuditd@gmail.com>
2016-10-08 17:26:39 -0700

    fixes client name for housing matche list

--- server/publish/housingMatch.js
+++ server/publish/housingMatch.js
@@ -12,14 +12,45 @@ Meteor.publish(
       housingMatch = HMISAPI.getHousingMatchForPublish();
       // according to the content received.
       // Adding Dummy Data for testing.
-      console.log(housingMatch);
+
+      for (let i = 0; i < housingMatch.length; i += 1) {
+        if (housingMatch[i].links) {
+          let schema = 'v2015';
+          if (housingMatch[i].links[1].href.indexOf('v2014') !== -1) {
+            schema = 'v2014';
+          }
+          housingMatch[i].eligibleClients.clientDetails = HMISAPI.getClient(
+            housingMatch[i].eligibleClients.clientId,
+            schema,
+            // useCurrentUserObject
+            false
+          );
+          housingMatch[i].eligibleClients.clientDetails.schema = schema;
+        }
+
+        // If client Id not found. So that we don't get any error.
+        if (!housingMatch[i].eligibleClients.clientDetails) {
+          const clientId = '';
+          const firstName = '';
+          const middleName = '';
+          const lastName = '';
+          const schema = '';
+          housingMatch[i].eligibleClients.clientDetails = {
+            clientId,
+            firstName,
+            middleName,
+            lastName,
+            schema,
+          };
+        }
+      }
+
     } else {
       HMISAPI.setCurrentUserId('');
     }
     if (housingMatch) {
       logger.info(`Publishing Housing Match: ${housingMatch.length}`);
       for (let i = 0; i < housingMatch.length; i += 1) {
-        // TODO Add client details (Name & link to profile) here.
         self.added('housingMatch', housingMatch[i].reservationId, housingMatch[i]);
       }
     }
