a188c3578d1b17a0e23e2b4475a3cf75d2e51bba
suryayadavalli <surya.yadavalli@gmail.com>
2016-10-12 22:10:52 +0530

    Build issue fix

--- client/events/client/viewClient.js
+++ client/events/client/viewClient.js
@@ -81,24 +81,14 @@ Template.viewClient.events(
 
       Router.go('selectSurvey', { _id: tmpl.data._id }, query);
     },
-	
-	    'click .btn-arrow-right'(event, tmpl){
+	  'click .btn-arrow-right'(event, tmpl) {
       logger.log('clicked status update' +event.currentTarget.dataset.step);
       const query = {};
-      const status = event.currentTarget.dataset.step ;
-
+      const status = event.currentTarget.dataset.step;
       const clientId = tmpl.data._id;
 
-      if (Router.current().params && Router.current().params.query
-        && Router.current().params.query.isHMISClient && Router.current().params.query.schema) {
-        query.query = {
-          isHMISClient: true,
-          schema: Router.current().params.query.schema,
-        };
-      }
-
       Meteor.call(
-        'updateClientMatchStatus',
+       'updateClientMatchStatus',
         clientId,
         status,
         (err, res) => {
@@ -111,6 +101,5 @@ Template.viewClient.events(
       );
 
     },
-	
   }
 );
