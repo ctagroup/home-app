406195d86e4ccf19be2ff2a0782fa0af7876c34a
Udit Desai <desaiuditd@gmail.com>
2016-10-28 03:40:44 -0700

    fixes HMIS Client for survey and its name. adds Survey button for local client

--- server/publish/responses.js
+++ server/publish/responses.js
@@ -12,13 +12,44 @@ Meteor.publish(
       const responseList = responses.find().fetch();
 
       for (let i = 0; i < responseList.length; i += 1) {
-        responseList[i].clientDetails = HMISAPI.getClient(
-          responseList[i].clientID,
-          responseList[i].clientSchema,
+        if (responseList[i].isHMISClient && responseList[i].clientSchema) {
+          responseList[i].clientDetails = HMISAPI.getClient(
+            responseList[i].clientID,
+            responseList[i].clientSchema,
+            // useCurrentUserObject
+            false
+          );
+        } else {
+          responseList[i].clientDetails = clients.findOne({ _id: responseList[i].clientID });
+        }
+        self.added('responses', responseList[i]._id, responseList[i]);
+      }
+    }
+
+    return self.ready();
+  }
+);
+
+Meteor.publish(
+  'singleResponse', function publishSingleResponse(responseId) {
+    const self = this;
+
+    if (self.userId && typeof responses !== 'undefined') {
+      HMISAPI.setCurrentUserId(self.userId);
+
+      const response = responses.findOne({ _id: responseId });
+
+      if (response && response.isHMISClient && response.clientSchema) {
+        response.clientDetails = HMISAPI.getClient(
+          response.clientID,
+          response.clientSchema,
           // useCurrentUserObject
           false
         );
-        self.added('responses', responseList[i]._id, responseList[i]);
+        self.added('responses', response._id, response);
+      } else if (response) {
+        response.clientDetails = clients.findOne({ _id: response.clientID });
+        self.added('responses', response._id, response);
       }
     }
 
