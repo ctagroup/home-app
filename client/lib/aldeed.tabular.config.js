0b7ba5692ccb927814157f641f13a2ac18439925
Udit Desai <desaiuditd@gmail.com>
2016-11-03 15:41:55 -0700

    fixes aldeed.tabular version update styles

---
+++ client/lib/aldeed.tabular.config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by udit on 03/11/16.
+ */
+
+import { $ } from 'meteor/jquery';
+import dataTablesBootstrap from 'datatables.net-bs';
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+dataTablesBootstrap(window, $);
