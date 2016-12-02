5565f0faf0f5eea6307d3420be91367412a61e57
Udit Desai <desaiuditd@gmail.com>
2016-12-01 21:10:43 -0800

    fixes edit user error & eslint issues

--- server/publish/userProfiles.js
+++ server/publish/userProfiles.js
@@ -20,11 +20,11 @@ Meteor.publish(
       userProfiles = response.profile;
       // according to the content received.
       logger.info(userProfiles.length);
-      for (let i = 0; (i * 30) < response.pagination.total && !stopFunction; i++) {
+      for (let i = 0; (i * 30) < response.pagination.total && !stopFunction; i += 1) {
         const temp = HMISAPI.getUserProfiles((i * 30), 30);
         userProfiles.push(...temp.profile);
         logger.info(`Temp: ${userProfiles.length}`);
-        for (let j = 0; j < temp.profile.length && !stopFunction; j++) {
+        for (let j = 0; j < temp.profile.length && !stopFunction; j += 1) {
           const tempItem = temp.profile[j];
           self.added('userProfiles', tempItem.id, tempItem);
           self.ready();
