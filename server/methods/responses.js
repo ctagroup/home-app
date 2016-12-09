b33028b2ec7c49ae7b593ccfa9c43f8d0fac36b8
fernandofcampos <fernandofcampos@gmail.com>
2016-12-09 01:16:12 -0200

    move response upload  to server method

--- server/methods/responses.js
+++ server/methods/responses.js
@@ -16,5 +16,54 @@ Meteor.methods(
     updateResponseStatus(_id, responsestatus) {
       responses.update(_id, { $set: { responsestatus } });
     },
+    uploadResponse(responseId) {
+      this.unblock();
+      // Checking if SPDAT or HUD. If SPDAT, then only upload.
+      try {
+        const response = responses.findOne({ _id: responseId });
+        const survey
+          = surveys.findOne({ _id: response.surveyID });
+        if (survey.stype !== 'hud') {
+          Meteor.call('updateResponseStatus', responseId, 'Uploading');
+
+          responseHmisHelpers.sendResponseToHmis(responseId, {}, true, (res) => {
+            if (res) {
+              // Calculate the scores now and send them too.
+              let score;
+              // Send response Id, survey Id and fromDb to true to score helpers.
+              switch (survey.stype) {
+                case 'spdat-t':
+                  score = spdatScoreHelpers.calcSpdatTayScore(survey._id, responseId, true);
+                  // upload the scores too.
+                  break;
+                case 'spdat-f':
+                  score = spdatScoreHelpers.calcSpdatFamilyScore(survey._id, responseId, true);
+                  break;
+                case 'spdat-s':
+                  score = spdatScoreHelpers.calcSpdatSingleScore(survey._id, responseId, true);
+                  break;
+                default:
+                  score = 0;
+                  // Should be other than VI-SPDAT.
+                  break;
+              }
+                  // On getting the scores, update them.
+              Meteor.call('sendScoresToHMIS', survey.apiSurveyServiceId,
+                                      response.clientID, score);
+
+              // save the submission Id.
+              Meteor.call('updateSubmissionIdForResponses', responseId, res.submissionId);
+            }
+          });
+        } else {
+          throw new Meteor.Error('Response upload for HUD not implemented');
+        }
+      } catch (e) {
+        logger.error(e);
+        throw (e);
+      } finally {
+        Meteor.call('updateResponseStatus', responseId, 'Completed');
+      }
+    },
   }
 );
