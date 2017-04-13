216209ebc7e0e07e4dcdf1213bd33e0946e3a788
pgorecki <przemyslaw.gorecki@gmail.com>
2017-04-13 09:58:09 +0200

    new client api now can add clients to hmis

--- imports/api/hmis-api/client-api.js
+++ imports/api/hmis-api/client-api.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { ApiEndpoint, HmisApiRegistry } from './api-registry';
 
 const CLIENT_BASE_URL = 'https://www.hmislynk.com/hmis-clientapi/rest';
@@ -8,12 +9,7 @@ export class ClientApi extends ApiEndpoint {
 
   getClients() {
     const url = `${CLIENT_BASE_URL}/clients/`;
-    const headers = {
-      'X-HMIS-TrustedApp-Id': this.appId,
-      Authorization: `HMISUserAuth session_token=${this.accessToken}`,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    };
+    const headers = this.getRequestHeaders();
     let response;
     try {
       response = HTTP.get(url, { headers }).data;
@@ -23,9 +19,44 @@ export class ClientApi extends ApiEndpoint {
     return response.Clients.clients;
   }
 
+  createClient(client, schema = 'v2015') {
+    const body = {
+      client: {
+        firstName: client.firstName,
+        middleName: client.middleName,
+        lastName: client.lastName,
+        nameSuffix: client.suffix,
+        nameDataQuality: 1,
+        ssn: client.ssn,
+        ssnDataQuality: 1,
+        dob: moment(client.dob).format('x'),
+        dobDataQuality: 1,
+        race: client.race,
+        ethnicity: client.ethnicity,
+        gender: client.gender,
+        // Putting otherGender as null. Confirmed with Javier. Because it's of no use as of now.
+        otherGender: 'null',
+        veteranStatus: client.veteranStatus,
+        sourceSystemId: client._id,
+        phoneNumber: client.phoneNumber,
+        emailAddress: client.emailAddress,
+      },
+    };
 
-  createClient() {
-    return 'abcd';
+    const headers = this.getRequestHeaders();
+    const url = `${CLIENT_BASE_URL}/${schema}/clients/`;
+
+    let result = false;
+    try {
+      const response = HTTP.post(url, {
+        data: body,
+        headers,
+      }).data;
+      result = response.client.clientId;
+    } catch (err) {
+      this.throwApiError(url, headers, err);
+    }
+    return result;
   }
 
   getClientFromUrl() {}
