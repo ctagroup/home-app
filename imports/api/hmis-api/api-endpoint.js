116db3dc79d3d4c4cbdd7102036f91f5d4cecc2f
pgorecki <przemyslaw.gorecki@gmail.com>
2017-04-14 09:33:46 +0200

    refactor hmis api

---
+++ imports/api/hmis-api/api-endpoint.js
@@ -0,0 +1,73 @@
+export class ApiEndpoint {
+  constructor(appId, accessToken) {
+    this.appId = appId;
+    this.accessToken = accessToken;
+  }
+
+  getRequestHeaders() {
+    return {
+      'X-HMIS-TrustedApp-Id': this.appId,
+      Authorization: `HMISUserAuth session_token=${this.accessToken}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    };
+  }
+
+  doGet(url) {
+    const headers = this.getRequestHeaders();
+    let response = false;
+    try {
+      response = HTTP.get(url, { headers });
+    } catch (err) {
+      this.throwApiError(url, headers, err);
+    }
+    return response.data;
+  }
+
+  doPost(url, data) {
+    const options = {
+      headers: this.getRequestHeaders(),
+      data,
+    };
+    let response = false;
+    try {
+      response = HTTP.post(url, options).data;
+    } catch (err) {
+      this.throwApiError(url, options, err);
+    }
+    return response;
+  }
+
+  doPut(url, data) {
+    const options = {
+      headers: this.getRequestHeaders(),
+      data,
+    };
+    let response = false;
+    try {
+      response = HTTP.put(url, options);
+    } catch (err) {
+      this.throwApiError(url, options, err);
+    }
+    return response.data;
+  }
+
+  throwApiError(url, requestHeaders, httpError) {
+    logger.error('api error', {
+      url,
+      requestHeaders,
+      httpError,
+    });
+
+    const code = httpError.response ? httpError.response.statusCode : 0;
+    let message = '';
+    try {
+      const content = JSON.parse(httpError.response.content);
+      message = content.errors.error[0].message;
+    } catch (err) {
+      message = httpError.message || 'An error has occurred';
+    }
+    throw new Meteor.Error('hmis.api', `${message} (${code})`);
+  }
+
+}
