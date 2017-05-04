c15f16b721769c0d4615df37fc9189a6034c60b2
pgorecki <przemyslaw.gorecki@gmail.com>
2017-05-04 08:58:16 +0200

    add caching to collections

---
+++ imports/both/cached-subscriptions.js
@@ -0,0 +1,15 @@
+/**
+ * Created by pgorecki on 04.05.17.
+ */
+
+// TODO: this should be moved to client-only code when router is moved to client as well
+
+import { SubsManager } from 'meteor/meteorhacks:subs-manager';
+
+export const CollectionsCountCache = new SubsManager();
+export const ClientsCache = new SubsManager();
+export const EligibleClientsCache = new SubsManager();
+export const HousingMatchCache = new SubsManager();
+export const HousingUnitsCache = new SubsManager();
+export const GlobalHouseholdsCache = new SubsManager();
+export const ProjectsCache = new SubsManager();
