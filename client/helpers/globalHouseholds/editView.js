5f29fbccdd507a4459c6dd8c4cefe820d83fbb30
pgorecki <przemyslaw@toptal.com>
2017-05-04 22:05:09 +0200

    move global households collection to imports

--- client/helpers/globalHouseholds/editView.js
+++ client/helpers/globalHouseholds/editView.js
@@ -1,11 +1,13 @@
 /**
  * Created by udit on 06/10/16.
  */
+import { GlobalHouseholds } from '/imports/api/global-households/global-households';
+
 
 Template.globalHouseholdEditView.helpers(
   {
     isActive(currentVal) {
-      const globalHousehold = globalHouseholds.findOne({ _id: Router.current().params._id });
+      const globalHousehold = GlobalHouseholds.findOne({ _id: Router.current().params._id });
       if (globalHousehold && globalHousehold.inactive === currentVal) {
         return 'checked';
       }
