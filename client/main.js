f57d1a71b6128d701fa3669b93d35e7d8d938d22
pgorecki <przemyslaw@toptal.com>
2017-08-04 09:02:10 +0200

    refactor eligible clients view

--- client/main.js
+++ client/main.js
@@ -2,6 +2,8 @@
  * Main entrypoint for the client app
  * All imports should go here
  */
+import '/imports/ui/eligibleClients';
 import '/imports/api/clients/methods';
 
 import '/imports/startup/client/routes';
+
