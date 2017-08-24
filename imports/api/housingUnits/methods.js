3ca40f0db37d0d7d02e1b5255e0341a95a76d76e
pgorecki <przemyslaw.gorecki@gmail.com>
2017-08-24 15:57:25 +0200

    refactor adding housing unit

--- imports/api/housingUnits/methods.js
+++ imports/api/housingUnits/methods.js
@@ -1,26 +1,29 @@
 import { HmisClient } from '/imports/api/hmis-api';
 import { logger } from '/imports/utils/logger';
 
-Meteor.methods(
-  {
-    'housingUnits.create'(housingObject) {
-      logger.info('creating housing unit', housingObject);
-      // TODO: permissions
-      return HMISAPI.createHousingUnit(housingObject);
-    },
-    'housingUnits.update'(id, housingObject) {
-      logger.info('updating housing unit', id, housingObject);
+Meteor.methods({
+  'housingUnits.create'(housingObject) {
+    logger.info('creating housing unit', housingObject);
+    // TODO: permissions
+    const hc = HmisClient.create(Meteor.userId());
+    return hc.api('housing').createHousingUnit(_.extend(housingObject, {
+      userId: Meteor.users.findOne(this.userId).services.HMIS.id,
+    }));
+  },
 
-      // TODO: permissions
-      const hc = HmisClient.create(Meteor.userId());
-      return hc.api('housing').updateHousingUnit(id, _.extend(housingObject, {
-        userId: Meteor.users.findOne(this.userId).services.HMIS.id,
-      }));
-    },
-    'housingUnits.delete'(id) {
-      logger.info('deleting housing unit', id);
-      // TODO: permissions
-      return HMISAPI.deleteHousingUnit(id);
-    },
-  }
-);
+  'housingUnits.update'(id, housingObject) {
+    logger.info('updating housing unit', id, housingObject);
+    // TODO: permissions
+    const hc = HmisClient.create(Meteor.userId());
+    return hc.api('housing').updateHousingUnit(id, _.extend(housingObject, {
+      userId: Meteor.users.findOne(this.userId).services.HMIS.id,
+    }));
+  },
+
+  'housingUnits.delete'(id) {
+    logger.info('deleting housing unit', id);
+    // TODO: permissions
+    const hc = HmisClient.create(Meteor.userId());
+    return hc.api('housing').deleteHousingUnit(id);
+  },
+});
