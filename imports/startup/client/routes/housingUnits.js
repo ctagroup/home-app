23957c298e69fd694c56df4a3f225e8f7fc0b015
pgorecki <przemyslaw.gorecki@gmail.com>
2017-08-24 16:03:33 +0200

    fix housing units router

--- imports/startup/client/routes/housingUnits.js
+++ imports/startup/client/routes/housingUnits.js
@@ -29,34 +29,10 @@ Router.route('adminDashboardhousingUnitsNew', {
   waitOn() {
     return ProjectsCache.subscribe('projects');
   },
-  action() {
-    this.render();
-  },
-  onBeforeAction() {
-    /*
-    if (collection.userRoles) {
-      if (!Roles.userIsInRole(Meteor.user(), collection.userRoles)) {
-        Router.go('notEnoughPermission');
-      }
-    }
-    */
-    this.next();
-  },
-  onAfterAction() {
-    /*
-    Session.set('admin_title', HomeDashboard.collectionLabel(collectionName));
-    Session.set('admin_subtitle', 'Create new');
-    Session.set('admin_collection_page', 'new');
-    Session.set('admin_collection_name', collectionName);
-    if (collection.templates && collection.templates.new
-        && collection.templates.new.onAfterAction) {
-      collection.templates.new.onAfterAction();
-    }
-    */
-  },
   data() {
     return {
-      admin_collection: Clients,
+      title: 'Housing Units',
+      subtitle: 'New',
     };
   },
 });
