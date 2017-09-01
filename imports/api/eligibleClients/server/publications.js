f2ee089b62670d41507f878cfc3eb93874d2c040
pgorecki <przemyslaw.gorecki@gmail.com>
2017-09-01 15:42:34 +0200

    limit max concurrent connections in eligible client publication

--- imports/api/eligibleClients/server/publications.js
+++ imports/api/eligibleClients/server/publications.js
@@ -1,3 +1,4 @@
+import { eachLimit } from 'async';
 import { HmisClient } from '/imports/api/hmis-api';
 import { logger } from '/imports/utils/logger';
 
@@ -23,38 +24,40 @@ Meteor.publish('eligibleClients.list', function publishEligibleClients() {
 
     // populate the list without the details
     for (let i = 0; i < eligibleClients.length && !stopFunction; i += 1) {
-      eligibleClients[i].clientDetails = {
-        clientId: '',
-        firstName: '',
-        middleName: '',
-        lastName: '',
-        schema: '',
-      };
+      eligibleClients[i].clientDetails = { loading: true };
       self.added('localEligibleClients', eligibleClients[i].clientId, eligibleClients[i]);
     }
     self.ready();
 
     // Add client details (Name & link to profile) here.
+    const queue = [];
     for (let i = 0; i < eligibleClients.length && !stopFunction; i += 1) {
+      let schema = 'v2015';
       if (eligibleClients[i].links && eligibleClients[i].links.length > 0) {
-        let schema = 'v2015';
         if (eligibleClients[i].links[0].href.indexOf('v2014') !== -1) {
           schema = 'v2014';
         }
-
-        hc.api('client').promiseGetClient(eligibleClients[i].clientId, schema)
-          .then((client) => {
-            const clientDetails = client;
-            clientDetails.schema = schema;
-            self.changed('localEligibleClients', eligibleClients[i].clientId, {
-              clientDetails,
-            });
-          })
-          .catch((e) => {
-            logger.error(e);
-          });
       }
+      queue.push({
+        clientId: eligibleClients[i].clientId,
+        schema,
+      });
     }
+
+    eachLimit(queue, Meteor.settings.connectionLimit, (data, callback) => {
+      Meteor.defer(() => {
+        const { clientId, schema } = data;
+        let clientDetails;
+        try {
+          clientDetails = hc.api('client').getClient(clientId, schema);
+          clientDetails.schema = schema;
+        } catch (e) {
+          clientDetails = { error: e.reason };
+        }
+        self.changed('localEligibleClients', clientId, { clientDetails });
+        callback();
+      });
+    });
   } catch (err) {
     logger.error('publish eligibleClients', err);
   }
