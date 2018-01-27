36e3da517a9fba2d222be5b3c0d53f993981439c
pgorecki <przemyslaw@toptal.com>
2018-01-27 15:38:04 +0100

    add cc recipients to mc211 emails

--- imports/api/mc211/methods.js
+++ imports/api/mc211/methods.js
@@ -20,6 +20,13 @@ Meteor.methods({
           <p>Sincerely,<br>United Way Monterey County</p>
         `,
         recipient: client.emailAddress,
+        ccRecipient: `
+          Edward.Simon@amwater.com,
+          Patrick.Pilz@amwater.com,
+          Catherine.Stedman@amwater.com,
+          Anthony.Lopez@amwater.com,
+          waterhelp@unitedwaymcca.org
+        `,
       };
       const additionalInfo = {
         messageType: 'mc211 approval',
@@ -51,6 +58,13 @@ Meteor.methods({
           <p>Sincerely,<br>United Way Monterey County</p>
         `,
         recipient: client.emailAddress,
+        ccRecipient: `
+          Edward.Simon@amwater.com,
+          Patrick.Pilz@amwater.com,
+          Catherine.Stedman@amwater.com,
+          Anthony.Lopez@amwater.com,
+          waterhelp@unitedwaymcca.org
+        `,
       };
       const additionalInfo = {
         messageType: 'mc211 rejection',
