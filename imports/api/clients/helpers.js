94f06f7340cdf66837b5bb6879ae9772a80139c0
Vladimir Kravchenko <art4ride@gmail.com>
2018-02-28 00:20:38 -0500

    Keep client versions, adjust load details method to improve readability/preselection for multiple versions load

--- imports/api/clients/helpers.js
+++ imports/api/clients/helpers.js
@@ -1,6 +1,10 @@
 import { removeEmpty } from '/imports/api/utils';
-export const mergeClient = (clientVersions) => {
-  let sortedClientSchemas = _.map(clientVersions, (version) => {
+
+const getVersions = (schemas) => _.map(schemas,
+  (version) => ({ schema: version.schema, clientId: version.clientId }));
+
+export const mergeClient = (clientVersionsList) => {
+  let sortedClientSchemas = _.map(clientVersionsList, (version) => {
     // assuming that link format is: '/hmis-clientapi/rest/{schema}/clients/{clientId}'
     const linkSchema = version.link.substring(21, 26);
     return Object.assign(removeEmpty(version), {
@@ -8,7 +12,8 @@ export const mergeClient = (clientVersions) => {
     });
   });
   sortedClientSchemas = _.sortBy(sortedClientSchemas, 'schema');
-  return Object.assign({}, ...sortedClientSchemas);
+  const clientVersions = getVersions(sortedClientSchemas);
+  return Object.assign({ clientVersions }, ...sortedClientSchemas);
 };
 
 export const mergeByDedupId = (hmisClients) => {
@@ -16,6 +21,7 @@ export const mergeByDedupId = (hmisClients) => {
   return _.map(_.values(groupedHMISClients), (clientSchemas) => {
     let sortedClientSchemas = _.sortBy(clientSchemas, 'schema');
     sortedClientSchemas = _.map(sortedClientSchemas, (client) => removeEmpty(client));
-    return Object.assign({}, ...sortedClientSchemas);
+    const clientVersions = getVersions(sortedClientSchemas);
+    return Object.assign({ clientVersions }, ...sortedClientSchemas);
   });
 };
--- imports/api/clients/server/publications.js
+++ imports/api/clients/server/publications.js
@@ -3,6 +3,85 @@ import { HmisClient } from '/imports/api/hmisApi';
 import { logger } from '/imports/utils/logger';
 import { mergeClient } from '/imports/api/clients/helpers';
 
+const getEligibleClient = (hc, clientId) => {
+  let eligibleClient;
+  try {
+    eligibleClient = hc.api('house-matching').getEligibleClient(clientId);
+  } catch (e) {
+    eligibleClient = { error: e.reason };
+  }
+  return eligibleClient;
+};
+
+const getClientEnrollments = (hc, clientId, schema, stopFunction) => {
+  const enrollments = hc.api('client').getClientEnrollments(clientId, schema);
+  for (let i = 0; i < enrollments.length && !stopFunction; i += 1) {
+    const exits = hc.api('client').getClientsEnrollmentExits(
+      clientId, enrollments[i].enrollmentId, schema
+    );
+    if (exits.length > 0) {
+      enrollments[i].exits = exits[0];
+    } else {
+      enrollments[i].exits = false;
+    }
+    enrollments[i].project = hc.api('client').getProject(enrollments[i].projectid, schema);
+  }
+  return enrollments;
+};
+
+const getGlobalHouseholds = (hc, clientId, schema, stopFunction) => {
+  const globalHouseholdMemberships = hc.api('global-household')
+  .getGlobalHouseholdMemberships(clientId);
+
+  const globalHouseholds = [];
+  for (let i = 0; i < globalHouseholdMemberships.length && !stopFunction; i += 1) {
+    const globalHousehold = hc.api('global-household').getHouseHold(
+      globalHouseholdMemberships[i].globalHouseholdId
+    );
+
+    if (globalHousehold) {
+      let hohSchema = 'v2015';
+      if (globalHousehold.links[0].rel.indexOf('v2014') !== -1) {
+        hohSchema = 'v2014';
+      }
+
+      globalHousehold.headOfHouseholdClient = hc.api('client').getClient(
+        globalHousehold.headOfHouseholdId,
+        hohSchema
+      );
+      globalHousehold.headOfHouseholdClient.schema = 'v2015';
+      globalHousehold.userDetails = hc.api('user-service').getUser(
+        globalHousehold.userId
+      );
+
+      globalHouseholds.push(globalHousehold);
+    }
+  }
+  return globalHouseholds;
+};
+
+const getReferralStatusHistory = (hc, clientId) => {
+  const referralStatusHistory = hc.api('house-matching').getReferralStatusHistory(clientId);
+  // Sort based on Timestamp
+  referralStatusHistory.sort((a, b) => {
+    const aTime = moment(a.dateUpdated, 'MM-DD-YYYY HH:mm:ss.SSS').unix();
+    const bTime = moment(b.dateUpdated, 'MM-DD-YYYY HH:mm:ss.SSS').unix();
+    return aTime - bTime;
+  });
+};
+
+const getHousingMatch = (hc, clientId) => {
+  const housingMatch = hc.api('house-matching').getHousingMatch(clientId);
+  const housingUnit = hc.api('housing').getHousingUnit(housingMatch.housingUnitId);
+  let projectSchema = 'v2015';
+  if (housingUnit.links && housingUnit.links.length > 0
+    && housingUnit.links[0].rel.indexOf('v2014') !== -1) {
+    projectSchema = 'v2014';
+  }
+  housingUnit.project = hc.api('client').getProject(housingUnit.projectId, projectSchema);
+  housingMatch.housingUnit = housingUnit;
+  return housingMatch;
+};
 
 Meteor.publish('clients.one', function pubClient(clientId, schema = 'v2015', loadDetails = true) {
   logger.info(`PUB[${this.userId}]: clients.one(${clientId}, ${schema})`);
@@ -28,85 +107,34 @@ Meteor.publish('clients.one', function pubClient(clientId, schema = 'v2015', loa
     // TODO [VK]: publish by dedupClientId directly
     const clientVersions = hc.api('client').searchClient(client.dedupClientId, 50);
 
-    self.added('localClients', client.clientId, mergeClient(clientVersions));
+    self.added('localClients', clientId, mergeClient(clientVersions));
     self.ready();
 
     if (loadDetails) {
       Meteor.setTimeout(() => {
-        let eligibleClient;
-        try {
-          eligibleClient = hc.api('house-matching').getEligibleClient(clientId);
-        } catch (e) {
-          eligibleClient = { error: e.reason };
-        }
-        self.changed('localClients', client.clientId, { eligibleClient });
+        const eligibleClient = getEligibleClient(hc, clientId);
+        self.changed('localClients', clientId, { eligibleClient });
       }, 0);
 
       Meteor.setTimeout(() => {
         try {
-          const enrollments = hc.api('client').getClientEnrollments(clientId, schema);
-          for (let i = 0; i < enrollments.length && !stopFunction; i += 1) {
-            const exits = hc.api('client').getClientsEnrollmentExits(
-              clientId, enrollments[i].enrollmentId, schema
-            );
-            if (exits.length > 0) {
-              enrollments[i].exits = exits[0];
-            } else {
-              enrollments[i].exits = false;
-            }
-            enrollments[i].project = hc.api('client').getProject(enrollments[i].projectid, schema);
-          }
-          self.changed('localClients', client.clientId, { enrollments });
+          const enrollments = getClientEnrollments(hc, clientId, schema, stopFunction);
+          self.changed('localClients', clientId, { enrollments });
         } catch (e) {
           logger.warn(e);
         }
       }, 0);
 
       Meteor.setTimeout(() => {
-        const globalHouseholdMemberships = hc.api('global-household')
-          .getGlobalHouseholdMemberships(clientId);
-
-        const globalHouseholds = [];
-        for (let i = 0; i < globalHouseholdMemberships.length && !stopFunction; i += 1) {
-          const globalHousehold = hc.api('global-household').getHouseHold(
-            globalHouseholdMemberships[i].globalHouseholdId
-          );
-
-          if (globalHousehold) {
-            let hohSchema = 'v2015';
-            if (globalHousehold.links[0].rel.indexOf('v2014') !== -1) {
-              hohSchema = 'v2014';
-            }
-
-            client = hc.api('client').getClient(clientId, schema);
-
-            globalHousehold.headOfHouseholdClient = hc.api('client').getClient(
-              globalHousehold.headOfHouseholdId,
-              hohSchema
-            );
-            globalHousehold.headOfHouseholdClient.schema = 'v2015';
-            globalHousehold.userDetails = hc.api('user-service').getUser(
-              globalHousehold.userId
-            );
-
-            globalHouseholds.push(globalHousehold);
-          }
-        }
-
-        self.changed('localClients', client.clientId, { globalHouseholds });
+        const globalHouseholds = getGlobalHouseholds(hc, clientId, schema, stopFunction);
+        self.changed('localClients', clientId, { globalHouseholds });
       }, 0);
 
       Meteor.setTimeout(() => {
         // fetch client status
         try {
-          const referralStatusHistory = hc.api('house-matching').getReferralStatusHistory(clientId);
-          // Sort based on Timestamp
-          referralStatusHistory.sort((a, b) => {
-            const aTime = moment(a.dateUpdated, 'MM-DD-YYYY HH:mm:ss.SSS').unix();
-            const bTime = moment(b.dateUpdated, 'MM-DD-YYYY HH:mm:ss.SSS').unix();
-            return aTime - bTime;
-          });
-          self.changed('localClients', client.clientId, { referralStatusHistory });
+          const referralStatusHistory = getReferralStatusHistory(hc, clientId);
+          self.changed('localClients', clientId, { referralStatusHistory });
         } catch (e) {
           logger.warn(e);
         }
@@ -114,16 +142,8 @@ Meteor.publish('clients.one', function pubClient(clientId, schema = 'v2015', loa
 
       Meteor.setTimeout(() => {
         try {
-          const housingMatch = hc.api('house-matching').getHousingMatch(clientId);
-          const housingUnit = hc.api('housing').getHousingUnit(housingMatch.housingUnitId);
-          let projectSchema = 'v2015';
-          if (housingUnit.links && housingUnit.links.length > 0
-            && housingUnit.links[0].rel.indexOf('v2014') !== -1) {
-            projectSchema = 'v2014';
-          }
-          housingUnit.project = hc.api('client').getProject(housingUnit.projectId, projectSchema);
-          housingMatch.housingUnit = housingUnit;
-          self.changed('localClients', client.clientId, { housingMatch });
+          const housingMatch = getHousingMatch(hc, clientId);
+          self.changed('localClients', clientId, { housingMatch });
         } catch (e) {
           logger.warn(e);
         }
@@ -132,7 +152,7 @@ Meteor.publish('clients.one', function pubClient(clientId, schema = 'v2015', loa
       Meteor.setTimeout(() => {
         try {
           const matchingScore = hc.api('house-matching').getClientScore(clientId);
-          self.changed('localClients', client.clientId, { matchingScore });
+          self.changed('localClients', clientId, { matchingScore });
         } catch (e) {
           logger.warn(e);
         }
@@ -145,8 +165,8 @@ Meteor.publish('clients.one', function pubClient(clientId, schema = 'v2015', loa
   self.ready();
 
   try {
-    Meteor.call('s3bucket.get', client.clientId, 'photo', (err, res) =>
-      self.changed('localClients', client.clientId, { photo: res })
+    Meteor.call('s3bucket.get', clientId, 'photo', (err, res) =>
+      self.changed('localClients', clientId, { photo: res })
     );
   } catch (e) {} // eslint-disable-line
 
