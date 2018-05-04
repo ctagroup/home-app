289bd7ab52bf272902689f73a17e4906f156e31c
Vladimir Kravchenko <art4ride@gmail.com>
2018-05-03 20:50:38 -0600

    Permission seeding

--- imports/startup/server/migrations/migratePermissions.js
+++ imports/startup/server/migrations/migratePermissions.js
@@ -1,3 +1,4 @@
+import RolePermissions from '/imports/api/rolePermissions/rolePermissions';
 import {
   ClientsAccessRoles,
   FilesAccessRoles,
@@ -6,34 +7,30 @@ import {
   PendingClientsAccessRoles,
   ResponsesAccessRoles,
 } from '/imports/config/permissions.js';
-// TODO [VK]: wrap into migration, set proper role-permission assignment
-ClientsAccessRoles.map((role) => {
-  // Add permission to role;
-  console.log('role', role);
-  return 'accessClients';
-});
-FilesAccessRoles.map((role) => {
-  // Add permission to role;
-  console.log('role', role);
-  return 'accessFiles';
-});
-GlobalHouseholdsAccessRoles.map((role) => {
-  // Add permission to role;
-  console.log('role', role);
-  return 'accessHouseholds';
-});
-HousingUnitsAccessRoles.map((role) => {
-  // Add permission to role;
-  console.log('role', role);
-  return 'accessHousingUnits';
-});
-PendingClientsAccessRoles.map((role) => {
-  // Add permission to role;
-  console.log('role', role);
-  return 'accessPendingClients';
-});
-ResponsesAccessRoles.map((role) => {
-  // Add permission to role;
-  console.log('role', role);
-  return 'accessResponses';
-});
+
+export const migratePermissions = () => {
+  ClientsAccessRoles.map((roleName) => {
+    const permissionName = 'accessClients';
+    return RolePermissions.insert({ roleName, permissionName });
+  });
+  FilesAccessRoles.map((roleName) => {
+    const permissionName = 'accessFiles';
+    return RolePermissions.insert({ roleName, permissionName });
+  });
+  GlobalHouseholdsAccessRoles.map((roleName) => {
+    const permissionName = 'accessHouseholds';
+    return RolePermissions.insert({ roleName, permissionName });
+  });
+  HousingUnitsAccessRoles.map((roleName) => {
+    const permissionName = 'accessHousingUnits';
+    return RolePermissions.insert({ roleName, permissionName });
+  });
+  PendingClientsAccessRoles.map((roleName) => {
+    const permissionName = 'accessPendingClients';
+    return RolePermissions.insert({ roleName, permissionName });
+  });
+  ResponsesAccessRoles.map((roleName) => {
+    const permissionName = 'accessResponses';
+    return RolePermissions.insert({ roleName, permissionName });
+  });
+};
