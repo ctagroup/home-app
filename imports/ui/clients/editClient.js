1ba0f5ccd25bbe720750815314af3f65029743f4
Vladimir Kravchenko <art4ride@gmail.com>
2018-08-18 17:57:36 +0200

    Edit client versions via dropdown

--- imports/ui/clients/editClient.js
+++ imports/ui/clients/editClient.js
@@ -1,11 +1,26 @@
+import { ReactiveVar } from 'meteor/reactive-var';
 import { Clients } from '/imports/api/clients/clients';
 import { RecentClients } from '/imports/api/recent-clients';
 import Alert from '/imports/ui/alert';
 import './editClient.html';
+import { getClientVersion } from '/imports/api/clients/helpers.js';
 
+// TODO: match by clientId+schema instead of just clientId?
+
+Template.editClient.onCreated(function editClientOnCreated() {
+  this.selected = new ReactiveVar(false);
+});
 
 Template.editClient.events(
   {
+    'change #version-select'(evt, tmpl) {
+      const selectedId = evt.target.value;
+      tmpl.selected.set(selectedId);
+    },
+    'click .versionItem'(evt, tmpl) {
+      const selectedId = this.value;
+      tmpl.selected.set(selectedId);
+    },
     'click .update': (evt, tmpl) => {
       evt.preventDefault();
       const data = {
@@ -27,18 +42,40 @@ Template.editClient.events(
 
       const client = tmpl.data.client;
       const methodName = client.clientId ? 'clients.update' : 'pendingClients.update';
-      const query = client.clientId ? { isHMISClient: true, schema: client.schema } : {};
 
       const { clientVersions } = client;
-      const { clientId, schema } = clientVersions && clientVersions.length ?
-        clientVersions[clientVersions.length - 1] : { clientId: client._id, schema: client.schema };
+      const selectedId = tmpl.selected.get();
+      let clientId;
+      let schema;
+      if (clientVersions && clientVersions.length) {
+        if (selectedId) {
+          const selectedVersion = _.find(clientVersions, (item) => item.clientId === selectedId);
+          clientId = selectedVersion.clientId;
+          schema = selectedVersion.schema;
+        } else {
+          clientId = clientVersions[clientVersions.length - 1].clientId;
+          schema = clientVersions[clientVersions.length - 1].schema;
+        }
+      } else {
+        clientId = client._id;
+        schema = client.schema;
+      }
 
       Meteor.call(methodName, clientId, data, schema, (error, result) => {
         if (error) {
           Alert.error(error);
         } else {
           Alert.success('Client updated');
-          Clients._collection.update(client._id, { $set: result }); // eslint-disable-line
+          if (selectedId) {
+            const moreData = _.map(result, (v, k) => {
+              if (['_id', 'schema', 'clientId'].includes(k)) return {};
+              return { [`${k}::${schema}::${selectedId}`]: v };
+            });
+            Clients._collection.update(client._id, { $set: Object.assign({}, ...moreData) }); // eslint-disable-line
+          } else {
+            Clients._collection.update(client._id, { $set: result }); // eslint-disable-line
+          }
+          const query = client.clientId ? { isHMISClient: true, schema: client.schema } : {};
           Router.go('viewClient', { _id: client._id }, { query });
         }
       });
@@ -64,3 +101,52 @@ Template.editClient.events(
     },
   }
 );
+
+const LATEST = 'Global Client';
+
+function versionOptions(clientVersions, selectedId) {
+  const options = clientVersions.map(({ schema, clientId }) => ({
+    value: clientId,
+    label: `${schema}-${clientId}`,
+    selected: clientId === selectedId,
+  }));
+
+  return [{
+    value: null,
+    label: LATEST,
+  }, ...options];
+}
+
+Template.editClient.helpers({
+  currentVersion() {
+    if (this && this.client) {
+      const selectedId = Template.instance() && Template.instance().selected.get();
+      const selected = versionOptions(this.client.clientVersions, selectedId)
+        .filter(p => p.selected);
+      if (selected.length) return selected[0].label;
+      return LATEST;
+    }
+    return LATEST;
+  },
+  options() {
+    if (this && this.client) {
+      const selectedId = Template.instance() && Template.instance().selected.get();
+      return versionOptions(this.client.clientVersions, selectedId);
+    }
+    return [{
+      value: null,
+      label: LATEST,
+    }];
+  },
+  selectedClient() {
+    const selectedId = Template.instance() && Template.instance().selected.get();
+    if (this && this.client && selectedId) {
+      const { clientId, schema } = _.find(this.client.clientVersions,
+          (item) => item.clientId === selectedId);
+      return getClientVersion(this.client, clientId, schema);
+    }
+    const currentClientId = Router.current().params._id;
+    const client = Clients.findOne(currentClientId);
+    return client;
+  },
+});
