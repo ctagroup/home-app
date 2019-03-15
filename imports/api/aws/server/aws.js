e96d441404be6f2bbc8a2a483e0a71df056fe27a
Przemysław Górecki <gorecki@example.com>
2019-03-15 15:04:28 +0100

    fix photo download bug, photo upload now uses dedupClientId

--- imports/api/aws/server/aws.js
+++ imports/api/aws/server/aws.js
@@ -10,6 +10,42 @@ const s3 = new AWS.S3({
 
 const SIGNATURES_PREFIX = 'consent signatures';
 
+function utf8ArrayToStr(array) {
+  const len = array.length;
+  let out = '';
+  let i = 0;
+  let c;
+  let char2;
+  let char3;
+
+  while (i < len) {
+    c = array[i++];
+    switch (c >> 4) {
+      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+        // 0xxxxxxx
+        out += String.fromCharCode(c);
+        break;
+      case 12: case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = array[i++];
+        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = array[i++];
+        char3 = array[i++];
+        out += String.fromCharCode(((c & 0x0F) << 12) |
+                      ((char2 & 0x3F) << 6) |
+                      ((char3 & 0x3F) << 0));
+        break;
+      default:
+        break;
+    }
+  }
+
+  return out;
+}
+
 Meteor.methods({
   's3bucket.get'(clientId, resource) {
     // TODO: permission check
@@ -20,11 +56,10 @@ Meteor.methods({
         if (err) {
           reject(err);
         } else {
-          resolve(data.Body);
+          resolve(utf8ArrayToStr(data.Body));
         }
       });
     })
-    .then(body => String.fromCharCode.apply(null, body))
     .await();
   },
   's3bucket.put'(clientId, resource, data) {
