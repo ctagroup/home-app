d62acbe40d96c43ea076918b09c68b3bee5bb1be
Przemysław Górecki <gorecki@example.com>
2019-04-17 13:47:07 +0200

    fix updating housing unit

--- imports/api/housingUnits/methods.js
+++ imports/api/housingUnits/methods.js
@@ -3,27 +3,27 @@ import { logger } from '/imports/utils/logger';
 
 Meteor.methods({
   'housingUnits.create'(housingObject) {
-    logger.info(`METHOD[${Meteor.userId()}]: housingUnits.create`, housingObject);
+    logger.info(`METHOD[${this.userId}]: housingUnits.create`, housingObject);
     // TODO: permissions
-    const hc = HmisClient.create(Meteor.userId());
+    const hc = HmisClient.create(this.userId);
     return hc.api('housing').createHousingUnit(_.extend(housingObject, {
       userId: Meteor.users.findOne(this.userId).services.HMIS.accountId,
     }));
   },
 
   'housingUnits.update'(id, housingObject) {
-    logger.info(`METHOD[${Meteor.userId()}]: housingUnits.update`, id, housingObject);
+    logger.info(`METHOD[${this.userId}]: housingUnits.update`, id, housingObject);
     // TODO: permissions
-    const hc = HmisClient.create(Meteor.userId());
+    const hc = HmisClient.create(this.userId);
     return hc.api('housing').updateHousingUnit(id, _.extend(housingObject, {
       userId: Meteor.users.findOne(this.userId).services.HMIS.accountId,
     }));
   },
 
   'housingUnits.delete'(id) {
-    logger.info(`METHOD[${Meteor.userId()}]: housingUnits.delete`, id);
+    logger.info(`METHOD[${this.userId}]: housingUnits.delete`, id);
     // TODO: permissions
-    const hc = HmisClient.create(Meteor.userId());
+    const hc = HmisClient.create(this.userId);
     return hc.api('housing').deleteHousingUnit(id);
   },
 });
