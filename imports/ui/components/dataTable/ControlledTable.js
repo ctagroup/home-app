6eb382b81e8acadb4d3684e2f9757aeb9401a13d
Vladimir Kravchenko <art4ride@gmail.com>
2019-04-30 11:29:30 -0500

    global searchbox for controlled DataTable

--- imports/ui/components/dataTable/ControlledTable.js
+++ imports/ui/components/dataTable/ControlledTable.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-import { transformColumn /* , formatDate */ } from './helpers.jsx';
+import { transformColumn } from './helpers.jsx';
+import { formatDate } from '/imports/both/helpers';
 // import _ from 'lodash';
 
 class ControlledTable extends Component {
@@ -20,12 +21,15 @@ class ControlledTable extends Component {
 
     const columns = (props || {}).options.columns.map(transformColumn);
     this.state = {
+      filter: '',
       columns,
       data: ((props || {}).data) || [],
       pages: null,
       loading: true,
     };
     this.fetchData = this.fetchData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.filterColumns = this.filterColumns.bind(this);
   }
 
   fetchData(state /* , instance*/) {
@@ -87,25 +91,75 @@ class ControlledTable extends Component {
     }
   }
 
+  filterColumns(input, columns, filter) {
+    if (!filter || filter === '') return input;
+    const matchColumns = columns.filter(({ data }) => data !== '_id');
+    const lowerCaseFilter = filter.toLowerCase();
+
+    console.log('input, columns, filter, matchColumns', input, columns, filter, matchColumns);
+    return input.filter(item => {
+      const out = matchColumns.map((column) =>
+          ({ filterMethod: column.filterMethod, column }))
+        .find(({ filterMethod, column }) => {
+          let data = item[column.data];
+          if (filterMethod) {
+            if (column.dataAccessor) data = column.dataAccessor(item);
+            console.log('filtering:', data, filterMethod({ value: lowerCaseFilter }, item, column));
+            return data !== undefined && filterMethod({ value: lowerCaseFilter }, item, column);
+          }
+          if (data instanceof Date) {
+            return formatDate(data).includes(lowerCaseFilter);
+          }
+          return data !== undefined && data.toString().toLowerCase().includes(lowerCaseFilter);
+        });
+      return out !== false && out !== undefined;
+    });
+  }
+
+  handleChange(event) {
+    const { target } = event;
+    const { value: inputValue = '' } = target;
+    this.setState({
+      filter: inputValue,
+    });
+  }
+
   render() {
     const { data, pages, loading, columns } = this.state;
     const minSize = Math.min(50, data.length);
     const defaultPageSize = [5, 10, 20, 25, 50, 100]
       .find((curr) => (curr - minSize) < 0);
+    const { globalFilter } = this.props;
+    const { filter } = this.state;
+    const filteredData = this.filterColumns(data, columns, filter);
     return (
-      <ReactTable
-        columns={columns}
-        // Forces table not to paginate or sort automatically, so we can handle it server-side:
-        manual
-        resizable={false}
-        data={data}
-        pages={pages} // Display the total number of pages
-        loading={loading} // Display the loading overlay when we need it
-        onFetchData={this.fetchData} // Request new data when things change
-        filterable
-        defaultPageSize={defaultPageSize}
-        className="-highlight"
-      />
+      <div style={{ padding: '0', margin: '0' }}>
+        {globalFilter && <div
+          id="DataTables_Table_0_filter" className="dataTables_filter"
+          style={{ display: 'inline-block', padding: '10px' }}
+        >
+          <label>Search:<input
+            type="search" className="form-control input-sm"
+            value={filter}
+            onChange={this.handleChange}
+            placeholder="" aria-controls="DataTables_Table_0"
+          />
+          </label>
+        </div>}
+        <ReactTable
+          columns={columns}
+          // Forces table not to paginate or sort automatically, so we can handle it server-side:
+          manual
+          resizable={false}
+          data={filteredData}
+          pages={pages} // Display the total number of pages
+          loading={loading} // Display the loading overlay when we need it
+          onFetchData={this.fetchData} // Request new data when things change
+          filterable={!globalFilter}
+          defaultPageSize={defaultPageSize}
+          className="-highlight"
+        />
+      </div>
     );
   }
 }
